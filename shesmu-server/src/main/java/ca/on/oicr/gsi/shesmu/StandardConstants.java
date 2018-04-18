339e6b7a3a35a238c61c2513ccf4cc168540db63
Andre Masella <andre.masella@oicr.on.ca>
2018-04-18 14:59:48 -0400

    Add constant for current time

--- shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/StandardConstants.java
+++ shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/StandardConstants.java
@@ -7,13 +7,25 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 import org.kohsuke.MetaInfServices;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.commons.GeneratorAdapter;
+import org.objectweb.asm.commons.Method;
 
 /**
  * Default constants provided to Shesmu scripts
  */
 @MetaInfServices(ConstantSource.class)
 public class StandardConstants implements ConstantSource {
-	private static final List<Constant> CONSTANTS = Arrays.asList(Constant.of("epoch", Instant.EPOCH));
+	private static final Type A_INSTANT_TYPE = Type.getType(Instant.class);
+	private static final List<Constant> CONSTANTS = Arrays.asList(Constant.of("epoch", Instant.EPOCH),
+			new Constant("now", Imyhat.DATE) {
+
+				@Override
+				protected void load(GeneratorAdapter methodGen) {
+					methodGen.invokeStatic(A_INSTANT_TYPE, METHOD_INSTANT__NOW);
+				}
+			});
+	private static final Method METHOD_INSTANT__NOW = new Method("now", A_INSTANT_TYPE, new Type[] {});
 
 	@Override
 	public Stream<Pair<String, Map<String, String>>> listConfiguration() {
