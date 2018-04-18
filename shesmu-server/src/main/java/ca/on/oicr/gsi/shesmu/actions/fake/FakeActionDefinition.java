1e60485df91a06c3d3e00a614a78b35cf5a3d74b
Andre Masella <andre.masella@oicr.on.ca>
2018-04-18 13:41:38 -0400

    Add “fake” actions mirrored from real instance

---
+++ shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/actions/fake/FakeActionDefinition.java
@@ -0,0 +1,28 @@
+package ca.on.oicr.gsi.shesmu.actions.fake;
+
+import java.util.stream.Stream;
+
+import org.objectweb.asm.Type;
+import org.objectweb.asm.commons.GeneratorAdapter;
+import org.objectweb.asm.commons.Method;
+
+import ca.on.oicr.gsi.shesmu.ActionDefinition;
+import ca.on.oicr.gsi.shesmu.ParameterDefinition;
+
+public class FakeActionDefinition extends ActionDefinition {
+	private static final Type A_DO_NOTHING = Type.getType(DoNothing.class);
+	private static final Method CTOR = new Method("<init>", Type.VOID_TYPE, new Type[] { Type.getType(String.class) });
+
+	public FakeActionDefinition(String name, Stream<ParameterDefinition> parameters) {
+		super(name, A_DO_NOTHING, parameters);
+	}
+
+	@Override
+	public void initialize(GeneratorAdapter methodGen) {
+		methodGen.newInstance(A_DO_NOTHING);
+		methodGen.dup();
+		methodGen.push(name());
+		methodGen.invokeConstructor(A_DO_NOTHING, CTOR);
+	}
+
+}
