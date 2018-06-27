367fa5b2fdb33d6688daa91cd257eff24c7f9640
evelii <Eve.Li@oicr.on.ca>
2018-06-27 13:56:41 -0400

    Created a subsample package and unit tests

---
+++ shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/subsample/FixedWithConditions.java
@@ -0,0 +1,33 @@
+package ca.on.oicr.gsi.shesmu.subsample;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FixedWithConditions<T> extends Subsampler<T> {
+
+	private final long numberOfItems;
+	private final Subsampler<T> parent;
+	private final Predicate<T> condition;
+
+	public FixedWithConditions(Subsampler<T> parent, long numberOfItems, Predicate<T> condition) {
+		this.parent = parent;
+		this.numberOfItems = numberOfItems;
+		this.condition = condition;
+		
+	}
+
+	@Override
+	protected int subsample(List<T> input, List<T> output) {
+		int position = parent.subsample(input, output);
+		int counter;
+		for(counter = 0; position + counter < input.size() && counter < numberOfItems; counter++) {
+			T item = input.get(position + counter);
+			if (!condition.test(item)) {
+				break;
+			}
+			output.add(item);
+		}
+		return position + counter;
+	}
+
+}
