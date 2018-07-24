c3785be7d2f9e4736ce04b917d51daeb4d4fee72
Andre Masella <andre@masella.name>
2018-07-23 22:26:10 -0400

    Switch to AutoUpdatingDirectory for PrometheusThrottler

--- shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/throttler/prometheus/PrometheusThrottler.java
+++ shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/throttler/prometheus/PrometheusThrottler.java
@@ -1,15 +1,12 @@
 package ca.on.oicr.gsi.shesmu.throttler.prometheus;
 
 import java.io.IOException;
-import java.time.Duration;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Collections;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -18,10 +15,12 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.kohsuke.MetaInfServices;
 
+import ca.on.oicr.gsi.shesmu.AutoUpdatingDirectory;
+import ca.on.oicr.gsi.shesmu.AutoUpdatingJsonFile;
+import ca.on.oicr.gsi.shesmu.Cache;
 import ca.on.oicr.gsi.shesmu.Pair;
 import ca.on.oicr.gsi.shesmu.RuntimeSupport;
 import ca.on.oicr.gsi.shesmu.Throttler;
-import io.prometheus.client.Gauge;
 
 /**
  * Determines if throttling should occur based on a Prometheus Alert Manager
@@ -32,57 +31,76 @@ import io.prometheus.client.Gauge;
  * AutoInhibit{environment="e",job="s"}
  * </pre>
  *
- * where <i>e</i> matches the environment variable <i>SHESMU_ENVIONMENT</i> (or
- * is absent) and <i>s</i> is one of the services specified by the action.
+ * where <i>e</i> matches the environment specified in the configuration file
+ * (or is absent) and <i>s</i> is one of the services specified by the action.
  */
 @MetaInfServices(Throttler.class)
 public class PrometheusThrottler implements Throttler {
+	private class Endpoint extends AutoUpdatingJsonFile<Configuration> {
+
+		private Optional<Configuration> configuration = Optional.empty();
+
+		public Endpoint(Path fileName) {
+			super(fileName, Configuration.class);
+		}
+
+		public Pair<String, Map<String, String>> configuration() {
+			final Map<String, String> properties = new TreeMap<>();
+			configuration.ifPresent(c -> {
+				properties.put("address", c.getAlertmanager());
+				properties.put("environment", c.getEnvironment());
+			});
+
+			return new Pair<>("Prometheus Throttler: " + fileName().toString(), properties);
+		}
+
+		public boolean isOverloaded(Set<String> services) {
+			return configuration.flatMap(configuration -> //
+			CACHE.get(configuration.getAlertmanager())//
+					.map(l -> l.stream()//
+							.anyMatch(alert -> alert.matches(configuration.getEnvironment(), services))))
+					.orElse(false);
+		}
+
+		@Override
+		protected Optional<Integer> update(Configuration value) {
+			configuration = Optional.of(value);
+			return Optional.empty();
+		}
+
+	}
+
+	private static Cache<String, List<AlertDto>> CACHE = new Cache<String, List<AlertDto>>("alertmanager", 5) {
+
+		@Override
+		protected List<AlertDto> fetch(String key) throws IOException {
+			try (CloseableHttpResponse response = HTTP_CLIENT
+					.execute(new HttpGet(String.format("%s/api/v1/alerts", key)))) {
+				final AlertResultDto result = RuntimeSupport.MAPPER.readValue(response.getEntity().getContent(),
+						AlertResultDto.class);
+				if (result == null || result.getData() == null) {
+					return null;
+				}
+				return result.getData();
+			}
+		}
+
+	};
+
 	private static final CloseableHttpClient HTTP_CLIENT = HttpClients.createDefault();
-	private static final Gauge lastUpdated = Gauge
-			.build("shesmu_prom_throttler_last_update", "The last time the data was fetched.").register();
 
-	private static final Gauge ok = Gauge
-			.build("shesmu_prom_throttler_status", "Whether the last call to Alert Manager was successful.").register();
-
-	private List<AlertDto> alerts = Collections.emptyList();
-
-	private final List<Configuration> configuration = RuntimeSupport.dataFiles(Configuration.class, ".alertman")
-			.collect(Collectors.toList());
-	private Instant lastUpdateTime = Instant.EPOCH;
+	private final AutoUpdatingDirectory<Endpoint> configuration = new AutoUpdatingDirectory<>(".alertman",
+			Endpoint::new);
 
 	@Override
 	public boolean isOverloaded(Set<String> services) {
 		return configuration.stream()//
-				.anyMatch(configuration -> {
-					if (Duration.between(lastUpdateTime, Instant.now()).get(ChronoUnit.SECONDS) > 300) {
-						try (CloseableHttpResponse response = HTTP_CLIENT.execute(
-								new HttpGet(String.format("%s/api/v1/alerts", configuration.getAlertmanager())))) {
-							final AlertResultDto result = RuntimeSupport.MAPPER
-									.readValue(response.getEntity().getContent(), AlertResultDto.class);
-							if (result == null || result.getData() == null) {
-								return false;
-							}
-							alerts = result.getData();
-							ok.set(1);
-							lastUpdateTime = Instant.now();
-							lastUpdated.setToCurrentTime();
-						} catch (final IOException e) {
-							e.printStackTrace();
-							ok.set(0);
-						}
-					}
-					return alerts.stream().anyMatch(alert -> alert.matches(configuration.getEnvironment(), services));
-				});
+				.anyMatch(ep -> ep.isOverloaded(services));
 	}
 
 	@Override
 	public Stream<Pair<String, Map<String, String>>> listConfiguration() {
-		return configuration.stream().map(configuration -> {
-			final Map<String, String> properties = new TreeMap<>();
-			properties.put("address", configuration.getAlertmanager());
-			properties.put("environment", configuration.getEnvironment());
-			return new Pair<>("Prometheus Throttler", properties);
-		});
+		return configuration.stream().map(Endpoint::configuration);
 	}
 
 }
