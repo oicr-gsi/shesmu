23d245109c2a7ce438602365e4a15885c9ab89fa
Andre Masella <andre.masella@oicr.on.ca>
2018-08-03 18:37:12 -0400

    Add equals and hashCode method for LimsKey
    This allows correct deduplication of `CellRangerAction`

--- plugin/seqware+pinery/src/main/java/ca/on/oicr/gsi/shesmu/variables/provenance/CellRangerAction.java
+++ plugin/seqware+pinery/src/main/java/ca/on/oicr/gsi/shesmu/variables/provenance/CellRangerAction.java
@@ -38,6 +38,67 @@ public class CellRangerAction extends SeqWareWorkflowAction<CellRangerAction.Cel
 			groupId = (String) t.get(3);
 		}
 
+		@SuppressWarnings("checkstyle:CyclomaticComplexity")
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (getClass() != obj.getClass()) {
+				return false;
+			}
+			final CellRangerLimsKey other = (CellRangerLimsKey) obj;
+			if (groupId == null) {
+				if (other.groupId != null) {
+					return false;
+				}
+			} else if (!groupId.equals(other.groupId)) {
+				return false;
+			}
+			if (ius_1 != other.ius_1) {
+				return false;
+			}
+			if (ius_2 == null) {
+				if (other.ius_2 != null) {
+					return false;
+				}
+			} else if (!ius_2.equals(other.ius_2)) {
+				return false;
+			}
+			if (lastModified == null) {
+				if (other.lastModified != null) {
+					return false;
+				}
+			} else if (!lastModified.toInstant().equals(other.lastModified.toInstant())) {
+				return false;
+			}
+			if (provider == null) {
+				if (other.provider != null) {
+					return false;
+				}
+			} else if (!provider.equals(other.provider)) {
+				return false;
+			}
+			if (sampleId == null) {
+				if (other.sampleId != null) {
+					return false;
+				}
+			} else if (!sampleId.equals(other.sampleId)) {
+				return false;
+			}
+			if (version == null) {
+				if (other.version != null) {
+					return false;
+				}
+			} else if (!version.equals(other.version)) {
+				return false;
+			}
+			return true;
+		}
+
 		@Override
 		public String getId() {
 			return sampleId;
@@ -56,6 +117,20 @@ public class CellRangerAction extends SeqWareWorkflowAction<CellRangerAction.Cel
 		@Override
 		public String getVersion() {
 			return version;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + (groupId == null ? 0 : groupId.hashCode());
+			result = prime * result + ius_1;
+			result = prime * result + (ius_2 == null ? 0 : ius_2.hashCode());
+			result = prime * result + (lastModified == null ? 0 : lastModified.toInstant().hashCode());
+			result = prime * result + (provider == null ? 0 : provider.hashCode());
+			result = prime * result + (sampleId == null ? 0 : sampleId.hashCode());
+			result = prime * result + (version == null ? 0 : version.hashCode());
+			return result;
 		}
 	}
 
