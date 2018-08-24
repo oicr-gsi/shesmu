47739d50f1a28e56cba4c92901a4f88777da2b04
Andre Masella <andre.masella@oicr.on.ca>
2018-08-24 16:42:24 -0400

    Add equals, hashCode, and toString methods to Pair

--- shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/Pair.java
+++ shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/Pair.java
@@ -74,11 +74,50 @@ public final class Pair<T, U> {
 		return function.apply(first, second);
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		@SuppressWarnings("rawtypes")
+		final Pair other = (Pair) obj;
+		if (first == null) {
+			if (other.first != null) {
+				return false;
+			}
+		} else if (!first.equals(other.first)) {
+			return false;
+		}
+		if (second == null) {
+			if (other.second != null) {
+				return false;
+			}
+		} else if (!second.equals(other.second)) {
+			return false;
+		}
+		return true;
+	}
+
 	/**
 	 * Get the first item in this pair
 	 */
 	public T first() {
 		return first;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (first == null ? 0 : first.hashCode());
+		result = prime * result + (second == null ? 0 : second.hashCode());
+		return result;
 	}
 
 	/**
@@ -104,4 +143,10 @@ public final class Pair<T, U> {
 	public boolean test(BiPredicate<? super T, ? super U> predicate) {
 		return predicate.test(first, second);
 	}
+
+	@Override
+	public String toString() {
+		return "<" + first + ", " + second + ">";
+	}
+
 }
