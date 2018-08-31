ae105c9cd88d981e40e0de5b86299dada1256b1d
Andre Masella <andre.masella@oicr.on.ca>
2018-08-31 12:38:09 -0400

    Fix incorrect Group Max/Min code

--- shesmu-server/src/test/java/ca/on/oicr/gsi/shesmu/RunTest.java
+++ shesmu-server/src/test/java/ca/on/oicr/gsi/shesmu/RunTest.java
@@ -101,7 +101,7 @@ public class RunTest {
 			new GsiStdValue("2", "/foo2", "text/x-nothing", "f031dcdb95c4ff2fbbc52a6be6c38117", 3, "SlowA", "aaa2",
 					new Tuple(1L, 2L, 3L), "the_foo_study", "unknown_sample", "that_guy",
 					new Tuple("RUN", 1L, "ACGTACGT"), "EX", "Fresh", "nn", "Frozen", "", "Inside", "", "", 300L,
-					"pointy", "UnsureSelect XT", Instant.EPOCH, new Tuple("SAM9000", "3.11", "miso, but less blue"),
+					"pointy", "UnsureSelect XT", Instant.EPOCH.plusSeconds(500), new Tuple("SAM9000", "3.11", "miso, but less blue"),
 					"test") };
 
 	private static final FunctionDefinition INT2DATE = new FunctionDefinition() {
