175c1cc7841cbcc48bfe67d9ad64ab3408976171
Andre Masella <andre.masella@oicr.on.ca>
2018-09-27 14:15:00 -0400

    Rename DoNothing to FakeAction

--- shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/actions/fake/FakeActionDefinition.java
+++ shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/actions/fake/FakeActionDefinition.java
@@ -10,19 +10,19 @@ import ca.on.oicr.gsi.shesmu.ActionDefinition;
 import ca.on.oicr.gsi.shesmu.ParameterDefinition;
 
 public class FakeActionDefinition extends ActionDefinition {
-	private static final Type A_DO_NOTHING = Type.getType(DoNothing.class);
+	private static final Type A_FAKE_ACTION_TYPE = Type.getType(FakeAction.class);
 	private static final Method CTOR = new Method("<init>", Type.VOID_TYPE, new Type[] { Type.getType(String.class) });
 
 	public FakeActionDefinition(String name, String description, Stream<ParameterDefinition> parameters) {
-		super(name, A_DO_NOTHING, "Fake version of: " + description, parameters);
+		super(name, A_FAKE_ACTION_TYPE, "Fake version of: " + description, parameters);
 	}
 
 	@Override
 	public void initialize(GeneratorAdapter methodGen) {
-		methodGen.newInstance(A_DO_NOTHING);
+		methodGen.newInstance(A_FAKE_ACTION_TYPE);
 		methodGen.dup();
 		methodGen.push(name());
-		methodGen.invokeConstructor(A_DO_NOTHING, CTOR);
+		methodGen.invokeConstructor(A_FAKE_ACTION_TYPE, CTOR);
 	}
 
 }
