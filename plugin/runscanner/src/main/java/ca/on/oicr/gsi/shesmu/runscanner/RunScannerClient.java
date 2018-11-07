95d37138e619eb4a5f2a1001e6b93f96bdc312d1
Andre Masella <andre.masella@oicr.on.ca>
2018-11-07 14:45:57 -0500

    Move Run Scanner plugin to new caching layer

--- plugin/runscanner/src/main/java/ca/on/oicr/gsi/shesmu/runscanner/RunScannerClient.java
+++ plugin/runscanner/src/main/java/ca/on/oicr/gsi/shesmu/runscanner/RunScannerClient.java
@@ -1,13 +1,12 @@
 package ca.on.oicr.gsi.shesmu.runscanner;
 
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.Instant;
 import java.util.Optional;
-import java.util.TreeMap;
 
 import javax.xml.stream.XMLStreamException;
 
+import org.apache.commons.codec.net.URLCodec;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -16,34 +15,73 @@ import org.apache.http.impl.client.HttpClients;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import ca.on.oicr.gsi.prometheus.LatencyHistogram;
 import ca.on.oicr.gsi.shesmu.runtime.RuntimeSupport;
 import ca.on.oicr.gsi.shesmu.util.AutoUpdatingJsonFile;
+import ca.on.oicr.gsi.shesmu.util.cache.KeyValueCache;
+import ca.on.oicr.gsi.shesmu.util.cache.SimpleRecord;
 import ca.on.oicr.gsi.shesmu.util.definitions.FileBackedConfiguration;
 import ca.on.oicr.gsi.shesmu.util.definitions.ShesmuMethod;
 import ca.on.oicr.gsi.shesmu.util.definitions.ShesmuParameter;
 import ca.on.oicr.gsi.status.ConfigurationSection;
 import ca.on.oicr.gsi.status.SectionRenderer;
-import io.prometheus.client.Counter;
 
-public final class RunScannerClient extends AutoUpdatingJsonFile<Configuration>
-		implements FileBackedConfiguration {
+public final class RunScannerClient extends AutoUpdatingJsonFile<Configuration> implements FileBackedConfiguration {
+	private class RunCache extends KeyValueCache<String, Optional<ObjectNode>> {
+
+		public RunCache(Path fileName) {
+			super("runscanner " + fileName.toString(), 15, SimpleRecord::new);
+		}
+
+		@Override
+		protected Optional<ObjectNode> fetch(String runName, Instant lastUpdated) throws Exception {
+			final HttpGet request = new HttpGet(String.format("%s/run/%s", url.get(), new URLCodec().encode(runName)));
+			try (CloseableHttpResponse response = HTTP_CLIENT.execute(request)) {
+				if (response.getStatusLine().getStatusCode() != 200) {
+					return Optional.empty();
+				}
+				return Optional
+						.of(RuntimeSupport.MAPPER.readValue(response.getEntity().getContent(), ObjectNode.class));
+			}
+		}
+
+	}
+
 	private static final CloseableHttpClient HTTP_CLIENT = HttpClients.createDefault();
-	private static final Counter requestErrors = Counter
-			.build("shesmu_runscanner_request_errors",
-					"The number of errors trying to countact the RunScanner web service.")
-			.labelNames("target").register();
 
-	private static final LatencyHistogram requestTime = new LatencyHistogram("shesmu_runscanner_request_time",
-			"The request time latency to access run information.", "target");
-	private final ConfigurationSection configurationPair;
-	private final Map<String, String> properties = new TreeMap<>();
-	private final Map<String, ObjectNode> runCache = new HashMap<>();
+	private final RunCache runCache;
 	private Optional<String> url = Optional.empty();
 
 	public RunScannerClient(Path fileName) {
 		super(fileName, Configuration.class);
-		configurationPair = new ConfigurationSection("RunScanner: " + fileName()) {
+		runCache = new RunCache(fileName);
+	}
+
+	@ShesmuMethod(description = "Get the serial number of the flowcell detected by the Run Scanner defined in {file}.")
+	public String $_flowcell(@ShesmuParameter(description = "name of run") String runName) {
+		return fetch(runName, "containerSerialNumber")//
+				.map(JsonNode::asText)//
+				.orElse("");
+	}
+
+	@ShesmuMethod(description = "Get the number of lanes detected by the Run Scanner defined in {file}.")
+	public long $_lane_count(@ShesmuParameter(description = "name of run") String runName) {
+		return fetch(runName, "laneCount")//
+				.filter(JsonNode::isIntegralNumber)//
+				.map(JsonNode::asLong)//
+				.orElse(-1L);
+	}
+
+	@ShesmuMethod(description = "Get the number of reads detected by the Run Scanner defined in {file}.")
+	public long $_read_ends(@ShesmuParameter(description = "name of run") String runName) {
+		return fetch(runName, "numReads")//
+				.filter(JsonNode::isIntegralNumber)//
+				.map(JsonNode::asLong)//
+				.orElse(-1L);
+	}
+
+	@Override
+	public ConfigurationSection configuration() {
+		return new ConfigurationSection(fileName().toString()) {
 
 			@Override
 			public void emit(SectionRenderer renderer) throws XMLStreamException {
@@ -53,78 +91,15 @@ public final class RunScannerClient extends AutoUpdatingJsonFile<Configuration>
 		};
 	}
 
-	@ShesmuMethod(description = "Get the serial number of the flowcell detected by the Run Scanner defined in {file}.")
-	public String $_flowcell(@ShesmuParameter(description = "name of run") String runName) {
-		return fetch(runName).map(run -> {
-			if (run.has("containerSerialNumber")) {
-				return run.get("containerSerialNumber").asText();
-			}
-			return "";
-		}).orElse("");
-	}
-
-	@ShesmuMethod(description = "Get the number of lanes detected by the Run Scanner defined in {file}.")
-	public long $_lane_count(@ShesmuParameter(description = "name of run") String runName) {
-		return fetch(runName).map(run -> {
-			if (run.has("laneCount")) {
-				final JsonNode laneCount = run.get("laneCount");
-				if (laneCount.isIntegralNumber()) {
-					final long count = laneCount.asLong();
-					return count;
-				}
-			}
-			return -1L;
-		}).orElse(-1L);
-	}
-
-	@ShesmuMethod(description = "Get the number of reads detected by the Run Scanner defined in {file}.")
-	public long $_read_ends(@ShesmuParameter(description = "name of run") String runName) {
-		return fetch(runName).map(run -> {
-			if (run.has("numReads")) {
-				final JsonNode laneCount = run.get("numReads");
-				if (laneCount.isIntegralNumber()) {
-					final long count = laneCount.asLong();
-					return count;
-				}
-			}
-			return -1L;
-		}).orElse(-1L);
-	}
-
-	@Override
-	public ConfigurationSection configuration() {
-		return configurationPair;
-	}
-
-	private Optional<ObjectNode> fetch(String runName) {
-		if (runCache.containsKey(runName)) {
-			return Optional.of(runCache.get(runName));
-		}
-		return url.flatMap(u -> {
-			final HttpGet request = new HttpGet(String.format("%s/run/%s", u, runName));
-			try (AutoCloseable timer = requestTime.start(u);
-					CloseableHttpResponse response = HTTP_CLIENT.execute(request)) {
-				if (response.getStatusLine().getStatusCode() != 200) {
-					requestErrors.labels(u).inc();
-					return Optional.empty();
-				}
-				final ObjectNode run = RuntimeSupport.MAPPER.readValue(response.getEntity().getContent(),
-						ObjectNode.class);
-				runCache.put(runName, run);
-				return Optional.of(run);
-			} catch (final Exception e) {
-				e.printStackTrace();
-				requestErrors.labels(u).inc();
-				return Optional.empty();
-			}
-		});
-
+	private Optional<JsonNode> fetch(String runName, String property) {
+		return runCache.get(runName)//
+				.filter(run -> run.has(property))//
+				.map(run -> run.get("laneCount"));
 	}
 
 	@Override
 	protected Optional<Integer> update(Configuration value) {
 		url = Optional.ofNullable(value.getUrl());
-		properties.put("url", value.getUrl());
 		return Optional.empty();
 
 	}
