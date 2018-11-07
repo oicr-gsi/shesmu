aa613d322cd8318fd56c1cea33e0eea527c8a678
Andre Masella <andre.masella@oicr.on.ca>
2018-11-07 14:40:51 -0500

    Use new caching layer in Pinery input source

--- plugin/niassa+pinery/src/main/java/ca/on/oicr/gsi/shesmu/pinery/PinerySource.java
+++ plugin/niassa+pinery/src/main/java/ca/on/oicr/gsi/shesmu/pinery/PinerySource.java
@@ -1,12 +1,11 @@
 package ca.on.oicr.gsi.shesmu.pinery;
 
-import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Collections;
-import java.util.List;
+import java.time.Instant;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.xml.stream.XMLStreamException;
 
@@ -14,41 +13,44 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import ca.on.oicr.gsi.shesmu.util.AutoUpdatingJsonFile;
-import ca.on.oicr.gsi.shesmu.util.Cache;
+import ca.on.oicr.gsi.shesmu.util.cache.MergingRecord;
+import ca.on.oicr.gsi.shesmu.util.cache.ValueCache;
 import ca.on.oicr.gsi.shesmu.util.definitions.FileBackedConfiguration;
 import ca.on.oicr.gsi.shesmu.util.definitions.ShesmuMethod;
 import ca.on.oicr.gsi.status.ConfigurationSection;
 import ca.on.oicr.gsi.status.SectionRenderer;
-import ca.on.oicr.pinery.client.HttpResponseException;
 import ca.on.oicr.pinery.client.PineryClient;
 import ca.on.oicr.ws.dto.SampleProjectDto;
 
 public class PinerySource extends AutoUpdatingJsonFile<ObjectNode> implements FileBackedConfiguration {
-	private static final Cache<String, List<SampleProjectDto>> PROJECTS = new Cache<String, List<SampleProjectDto>>(
-			"pinery_projects", 3600) {
+	private class ProjectCache extends ValueCache<Stream<SampleProjectDto>> {
+
+		public ProjectCache(Path fileName) {
+			super("pinery_projects " + fileName.toString(), 3600, MergingRecord.by(SampleProjectDto::getName));
+		}
 
 		@Override
-		protected List<SampleProjectDto> fetch(String url) throws IOException {
-			try (final PineryClient client = new PineryClient(url)) {
-				return client.getSampleProject().all();
-			} catch (final HttpResponseException e) {
-				e.printStackTrace();
-				return null;
+		protected Stream<SampleProjectDto> fetch(Instant lastUpdated) throws Exception {
+			if (!url.isPresent())
+				return Stream.empty();
+			try (final PineryClient client = new PineryClient(url.get())) {
+				return client.getSampleProject().all().stream();
 			}
 		}
 
 	};
 
 	private Optional<String> url = Optional.empty();
+	private final ProjectCache projects;
 
 	public PinerySource(Path fileName) {
 		super(fileName, ObjectNode.class);
+		projects = new ProjectCache(fileName);
 	}
 
 	@ShesmuMethod(name = "$_active_projects", type = "as", description = "Projects marked active from in Pinery defined in {file}.")
 	public Set<String> activeProjects() {
-		return url.flatMap(PROJECTS::get).orElse(Collections.emptyList())//
-				.stream()//
+		return projects.get()//
 				.filter(SampleProjectDto::isActive)//
 				.map(SampleProjectDto::getName)//
 				.collect(Collectors.toSet());
@@ -56,15 +58,14 @@ public class PinerySource extends AutoUpdatingJsonFile<ObjectNode> implements Fi
 
 	@ShesmuMethod(name = "$_projects", type = "as", description = "All projects from in Pinery defined in {file}.")
 	public Set<String> allProjects() {
-		return url.flatMap(PROJECTS::get).orElse(Collections.emptyList())//
-				.stream()//
+		return projects.get()//
 				.map(SampleProjectDto::getName)//
 				.collect(Collectors.toSet());
 	}
 
 	@Override
 	public ConfigurationSection configuration() {
-		return new ConfigurationSection("Pinery Functions: " + fileName()) {
+		return new ConfigurationSection(fileName().toString()) {
 
 			@Override
 			public void emit(SectionRenderer renderer) throws XMLStreamException {
@@ -76,6 +77,7 @@ public class PinerySource extends AutoUpdatingJsonFile<ObjectNode> implements Fi
 	@Override
 	protected Optional<Integer> update(ObjectNode value) {
 		url = Optional.ofNullable(value.get("url")).map(JsonNode::asText);
+		projects.invalidate();
 		return Optional.empty();
 	}
 }
