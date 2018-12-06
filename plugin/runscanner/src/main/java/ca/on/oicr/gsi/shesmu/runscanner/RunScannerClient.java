c2833e49149a138efb4760eca9e932596e62c0c3
Andre Masella <andre.masella@oicr.on.ca>
2018-12-06 09:46:22 -0500

    Use RunScanner DTO package

--- plugin/runscanner/src/main/java/ca/on/oicr/gsi/shesmu/runscanner/RunScannerClient.java
+++ plugin/runscanner/src/main/java/ca/on/oicr/gsi/shesmu/runscanner/RunScannerClient.java
@@ -12,9 +12,8 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
+import ca.on.oicr.gsi.runscanner.dto.IlluminaNotificationDto;
+import ca.on.oicr.gsi.runscanner.dto.NotificationDto;
 import ca.on.oicr.gsi.shesmu.runtime.RuntimeSupport;
 import ca.on.oicr.gsi.shesmu.util.AutoUpdatingJsonFile;
 import ca.on.oicr.gsi.shesmu.util.cache.KeyValueCache;
@@ -26,14 +25,14 @@ import ca.on.oicr.gsi.status.ConfigurationSection;
 import ca.on.oicr.gsi.status.SectionRenderer;
 
 public final class RunScannerClient extends AutoUpdatingJsonFile<Configuration> implements FileBackedConfiguration {
-	private class RunCache extends KeyValueCache<String, Optional<ObjectNode>> {
+	private class RunCache extends KeyValueCache<String, Optional<NotificationDto>> {
 
 		public RunCache(Path fileName) {
 			super("runscanner " + fileName.toString(), 15, SimpleRecord::new);
 		}
 
 		@Override
-		protected Optional<ObjectNode> fetch(String runName, Instant lastUpdated) throws Exception {
+		protected Optional<NotificationDto> fetch(String runName, Instant lastUpdated) throws Exception {
 			if (!url.isPresent()) {
 				return Optional.empty();
 			}
@@ -42,8 +41,10 @@ public final class RunScannerClient extends AutoUpdatingJsonFile<Configuration> 
 				if (response.getStatusLine().getStatusCode() != 200) {
 					return Optional.empty();
 				}
-				return Optional
-						.of(RuntimeSupport.MAPPER.readValue(response.getEntity().getContent(), ObjectNode.class));
+				NotificationDto run = RuntimeSupport.MAPPER.readValue(response.getEntity().getContent(),
+						NotificationDto.class);
+				run.setMetrics(null); // Discard metrics to save memory
+				return Optional.of(run);
 			}
 		}
 
@@ -61,24 +62,23 @@ public final class RunScannerClient extends AutoUpdatingJsonFile<Configuration> 
 
 	@ShesmuMethod(description = "Get the serial number of the flowcell detected by the Run Scanner defined in {file}.")
 	public String $_flowcell(@ShesmuParameter(description = "name of run") String runName) {
-		return fetch(runName, "containerSerialNumber")//
-				.map(JsonNode::asText)//
+		return runCache.get(runName)//
+				.map(NotificationDto::getContainerSerialNumber)//
 				.orElse("");
 	}
 
 	@ShesmuMethod(description = "Get the number of lanes detected by the Run Scanner defined in {file}.")
 	public long $_lane_count(@ShesmuParameter(description = "name of run") String runName) {
-		return fetch(runName, "laneCount")//
-				.filter(JsonNode::isIntegralNumber)//
-				.map(JsonNode::asLong)//
+		return runCache.get(runName)//
+				.map(r -> (long) r.getLaneCount())//
 				.orElse(-1L);
 	}
 
 	@ShesmuMethod(description = "Get the number of reads detected by the Run Scanner defined in {file}.")
 	public long $_read_ends(@ShesmuParameter(description = "name of run") String runName) {
-		return fetch(runName, "numReads")//
-				.filter(JsonNode::isIntegralNumber)//
-				.map(JsonNode::asLong)//
+		return runCache.get(runName)//
+				.filter(IlluminaNotificationDto.class::isInstance)
+				.map(r -> (long) ((IlluminaNotificationDto) r).getNumReads())//
 				.orElse(-1L);
 	}
 
@@ -92,12 +92,6 @@ public final class RunScannerClient extends AutoUpdatingJsonFile<Configuration> 
 				url.ifPresent(u -> renderer.link("URL", u, u));
 			}
 		};
-	}
-
-	private Optional<JsonNode> fetch(String runName, String property) {
-		return runCache.get(runName)//
-				.filter(run -> run.has(property))//
-				.map(run -> run.get("laneCount"));
 	}
 
 	@Override
