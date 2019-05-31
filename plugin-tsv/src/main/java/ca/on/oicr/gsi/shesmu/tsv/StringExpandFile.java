5f9c0b90a13cdd183b96be577c93109ed3640cc6
Andre Masella <andre.masella@oicr.on.ca>
2019-05-31 13:25:01 -0400

    Allow doing string expansion for 10X barcodes

---
+++ plugin-tsv/src/main/java/ca/on/oicr/gsi/shesmu/tsv/StringExpandFile.java
@@ -0,0 +1,71 @@
+package ca.on.oicr.gsi.shesmu.tsv;
+
+import ca.on.oicr.gsi.shesmu.plugin.Definer;
+import ca.on.oicr.gsi.shesmu.plugin.PluginFile;
+import ca.on.oicr.gsi.shesmu.plugin.functions.ShesmuMethod;
+import ca.on.oicr.gsi.shesmu.plugin.functions.ShesmuParameter;
+import ca.on.oicr.gsi.status.SectionRenderer;
+import io.prometheus.client.Gauge;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class StringExpandFile extends PluginFile {
+
+  private static final Pattern TAB = Pattern.compile("\t");
+  private static final Gauge tableBad =
+      Gauge.build("shesmu_strexpand_lookup_bad", "A string expansion table is badly formed.")
+          .labelNames("fileName")
+          .register();
+  private final Definer definer;
+  private Map<String, Set<String>> expansions = Collections.emptyMap();
+  private boolean good;
+
+  public StringExpandFile(Path fileName, String instanceName, Definer<StringExpandFile> definer) {
+    super(fileName, instanceName);
+    this.definer = definer;
+  }
+
+  @Override
+  public void configuration(SectionRenderer renderer) {
+    renderer.line("Is valid?", good ? "Yes" : "No");
+  }
+
+  @ShesmuMethod(
+      name = "$",
+      type = "as",
+      description =
+          "Expand a string to a set of strings or a set containing only the input string.")
+  public Set<String> get(
+      @ShesmuParameter(
+              description =
+                  "The input string to find the in the table; if it does not exist in the table, it is returned as a list containing the input")
+          String input) {
+    return expansions.getOrDefault(input, Collections.singleton(input));
+  }
+
+  @Override
+  public Optional<Integer> update() {
+    good = false;
+    try (Stream<String> lines = Files.lines(fileName(), StandardCharsets.UTF_8)) {
+      expansions =
+          lines
+              .map(String::trim)
+              .filter(l -> !l.startsWith("#"))
+              .map(TAB::split)
+              .collect(
+                  Collectors.toMap(
+                      x -> x[0], x -> new TreeSet<>(Arrays.asList(x).subList(1, x.length))));
+      good = true;
+    } catch (final Exception e) {
+      good = false;
+      e.printStackTrace();
+    }
+    tableBad.labels(fileName().toString()).set(good ? 0 : 1);
+    return Optional.empty();
+  }
+}
