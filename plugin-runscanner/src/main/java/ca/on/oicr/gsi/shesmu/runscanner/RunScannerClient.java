8da6c3c219eaddafad612c740f589675f0bfb81f
Andre Masella <andre.masella@oicr.on.ca>
2019-08-06 15:49:09 -0400

    Add a function to compute the Illumina flowcell layout

--- plugin-runscanner/src/main/java/ca/on/oicr/gsi/shesmu/runscanner/RunScannerClient.java
+++ plugin-runscanner/src/main/java/ca/on/oicr/gsi/shesmu/runscanner/RunScannerClient.java
@@ -1,7 +1,9 @@
 package ca.on.oicr.gsi.shesmu.runscanner;
 
+import ca.on.oicr.gsi.Pair;
 import ca.on.oicr.gsi.runscanner.dto.IlluminaNotificationDto;
 import ca.on.oicr.gsi.runscanner.dto.NotificationDto;
+import ca.on.oicr.gsi.runscanner.dto.type.IlluminaChemistry;
 import ca.on.oicr.gsi.shesmu.plugin.cache.InitialCachePopulationException;
 import ca.on.oicr.gsi.shesmu.plugin.cache.KeyValueCache;
 import ca.on.oicr.gsi.shesmu.plugin.cache.SimpleRecord;
@@ -13,7 +15,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.nio.file.Path;
 import java.time.Instant;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 import javax.xml.stream.XMLStreamException;
 import org.apache.commons.codec.net.URLCodec;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -22,13 +27,6 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 public final class RunScannerClient extends JsonPluginFile<Configuration> {
-  static final CloseableHttpClient HTTP_CLIENT = HttpClients.createDefault();
-  private static final ObjectMapper MAPPER = new ObjectMapper();
-
-  static {
-    MAPPER.registerModule(new JavaTimeModule());
-  }
-
   private class RunCache extends KeyValueCache<String, Optional<NotificationDto>> {
 
     public RunCache(Path fileName) {
@@ -55,6 +53,54 @@ public final class RunScannerClient extends JsonPluginFile<Configuration> {
     }
   }
 
+  /**
+   * Determine the correct flow cell geometry for a run
+   *
+   * @param notificationDto the run description
+   */
+  private static Set<Set<Long>> computeGeometry(NotificationDto notificationDto) {
+    if (!(notificationDto instanceof IlluminaNotificationDto)) {
+      return Collections.emptySet();
+    }
+    final IlluminaNotificationDto run = (IlluminaNotificationDto) notificationDto;
+    // If chemistry is NextSeq or HiSeq onboard clustering or NovaSeq Standard
+    final boolean isJoined =
+        run.getChemistry() == IlluminaChemistry.NS_HIGH
+            || run.getChemistry() == IlluminaChemistry.NS_MID
+            || run.getWorkflowType() != null
+                && (run.getWorkflowType().equals("NovaSeqStandard")
+                    || run.getWorkflowType().equals("OnBoardClustering"));
+    return LANE_GEOMETRY_CACHE.computeIfAbsent(
+        new Pair<>(run.getLaneCount(), isJoined), RunScannerClient::createFlowCellLayout);
+  }
+
+  /**
+   * Create a set of sets, where all lanes that are merged are in a set together
+   *
+   * @param format the number of lanes and whether the lanes are joined or not
+   */
+  private static Set<Set<Long>> createFlowCellLayout(Pair<Integer, Boolean> format) {
+    if (format.second()) {
+      return Collections.singleton(
+          LongStream.rangeClosed(1, format.first())
+              .boxed()
+              .collect(Collectors.toCollection(TreeSet::new)));
+    } else {
+      return LongStream.rangeClosed(1, format.first())
+          .mapToObj(Collections::singleton)
+          .collect(Collectors.toSet());
+    }
+  }
+
+  static final CloseableHttpClient HTTP_CLIENT = HttpClients.createDefault();
+  private static final Map<Pair<Integer, Boolean>, Set<Set<Long>>> LANE_GEOMETRY_CACHE =
+      new ConcurrentHashMap<>();
+  private static final ObjectMapper MAPPER = new ObjectMapper();
+
+  static {
+    MAPPER.registerModule(new JavaTimeModule());
+  }
+
   private final RunCache runCache;
   private Optional<String> url = Optional.empty();
 
@@ -71,6 +117,21 @@ public final class RunScannerClient extends JsonPluginFile<Configuration> {
       return runCache.get(runName).map(NotificationDto::getContainerSerialNumber).orElse("");
     } catch (InitialCachePopulationException e) {
       return "";
+    }
+  }
+
+  @ShesmuMethod(
+      description =
+          "Get the lane splitting/merging layout of the flowcell detected by the Run Scanner defined in {file}.")
+  public Set<Set<Long>> $_flowcell_geometry(
+      @ShesmuParameter(description = "name of run") String runName) {
+    try {
+      return runCache
+          .get(runName)
+          .map(RunScannerClient::computeGeometry)
+          .orElse(Collections.emptySet());
+    } catch (InitialCachePopulationException e) {
+      return Collections.emptySet();
     }
   }
 
