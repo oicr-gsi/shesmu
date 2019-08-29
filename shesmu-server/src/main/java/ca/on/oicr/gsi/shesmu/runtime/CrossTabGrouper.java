76a53a016be97a4c4ad3a054de2f4420965b7621
Andre Masella <andre.masella@oicr.on.ca>
2019-08-29 13:49:04 -0400

    Allow building a cross tab grouper

---
+++ shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/runtime/CrossTabGrouper.java
@@ -0,0 +1,50 @@
+package ca.on.oicr.gsi.shesmu.runtime;
+
+import ca.on.oicr.gsi.shesmu.plugin.grouper.Grouper;
+import ca.on.oicr.gsi.shesmu.plugin.grouper.Subgroup;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CrossTabGrouper<I, T, O> implements Grouper<I, O> {
+  private final Function<I, T> partition;
+  private final Supplier<BiConsumer<O, I>> collectorSupplier;
+
+  public CrossTabGrouper(Function<I, T> partition, Supplier<BiConsumer<O, I>> collectorSupplier) {
+    this.partition = partition;
+    this.collectorSupplier = collectorSupplier;
+  }
+
+  @Override
+  public Stream<Subgroup<I, O>> group(List<I> inputs) {
+    // Split all the input up by partition
+    final Map<T, List<I>> partitions =
+        inputs.stream().collect(Collectors.groupingBy(partition, Collectors.toList()));
+
+    // Create a stream of things that create a new subgroup
+    Stream<Supplier<Subgroup<I, O>>> stream =
+        Stream.of(() -> new Subgroup<>(collectorSupplier.get()));
+    // For each partition, take all the previously produced subgroups and cross them with each input
+    // item
+    for (final List<I> partition : partitions.values()) {
+      stream =
+          stream.flatMap(
+              supplier ->
+                  partition
+                      .stream()
+                      .map(
+                          item ->
+                              () -> {
+                                final Subgroup<I, O> subgroup = supplier.get();
+                                subgroup.add(item);
+                                return subgroup;
+                              }));
+    }
+    // Now, produce all those aggregate groups
+    return stream.map(Supplier::get);
+  }
+}
