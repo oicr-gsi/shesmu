b31efd2d11c825677b8eca157c92254eb2424f92
Andre Masella <andre.masella@oicr.on.ca>
2019-09-12 13:41:09 -0400

    Reduce queuing on caches by serving stale data

--- shesmu-pluginapi/src/main/java/ca/on/oicr/gsi/shesmu/plugin/cache/MergingRecord.java
+++ shesmu-pluginapi/src/main/java/ca/on/oicr/gsi/shesmu/plugin/cache/MergingRecord.java
@@ -1,10 +1,8 @@
 package ca.on.oicr.gsi.shesmu.plugin.cache;
 
-import java.time.Duration;
 import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.function.BiFunction;
 import java.util.function.Function;
@@ -18,7 +16,7 @@ import java.util.stream.Stream;
  * @param <V> the type of the items
  * @param <I> the type of the ids
  */
-public final class MergingRecord<V, I> implements Record<Stream<V>> {
+public final class MergingRecord<V, I> extends BaseRecord<Stream<V>, List<V>> {
 
   /** Merge records by the supplied id */
   public static <V, I> BiFunction<Owner, Updater<Stream<V>>, Record<Stream<V>>> by(
@@ -26,61 +24,36 @@ public final class MergingRecord<V, I> implements Record<Stream<V>> {
     return (owner, fetcher) -> new MergingRecord<>(owner, fetcher, getId);
   }
 
-  private Instant fetchTime = Instant.EPOCH;
   private final Updater<Stream<V>> fetcher;
   private final Function<V, I> getId;
-  private boolean initialState = true;
-  private final Owner owner;
-  private List<V> value;
 
   public MergingRecord(Owner owner, Updater<Stream<V>> fetcher, Function<V, I> getId) {
-    this.owner = owner;
+    super(owner, Collections.emptyList());
     this.fetcher = fetcher;
     this.getId = getId;
-    this.value = Collections.emptyList();
   }
 
   @Override
-  public int collectionSize() {
-    return value.size();
+  protected int collectionSize(List<V> state) {
+    return state.size();
   }
 
   @Override
-  public void invalidate() {
-    fetchTime = Instant.EPOCH;
+  protected Stream<V> unpack(List<V> state) {
+    return state.stream();
   }
 
   @Override
-  public Instant lastUpdate() {
-    return fetchTime;
-  }
-
-  @Override
-  public synchronized Stream<V> refresh() {
-    final Instant now = Instant.now();
-    if (Duration.between(fetchTime, now).toMinutes() > owner.ttl()) {
-      try (AutoCloseable timer = refreshLatency.start(owner.name())) {
-        final Stream<V> stream =
-            Optional.ofNullable(fetcher.update(fetchTime)).orElse(Stream.empty());
-        final List<V> buffer = stream.collect(Collectors.toList());
-        stream.close();
-        final Set<I> newIds = buffer.stream().map(getId).collect(Collectors.toSet());
-        value =
-            Stream.concat(
-                    value.stream().filter(item -> !newIds.contains(getId.apply(item))),
-                    buffer.stream())
-                .collect(Collectors.toList());
-
-        fetchTime = Instant.now();
-        initialState = false;
-      } catch (final Exception e) {
-        e.printStackTrace();
-        staleRefreshError.labels(owner.name()).inc();
-      }
+  protected List<V> update(List<V> oldstate, Instant fetchTime) throws Exception {
+    final Stream<V> stream = fetcher.update(fetchTime);
+    if (stream == null) {
+      return null;
     }
-    if (initialState) {
-      throw new InitialCachePopulationException();
-    }
-    return value.stream();
+    final List<V> buffer = stream.collect(Collectors.toList());
+    stream.close();
+    final Set<I> newIds = buffer.stream().map(getId).collect(Collectors.toSet());
+    return Stream.concat(
+            oldstate.stream().filter(item -> !newIds.contains(getId.apply(item))), buffer.stream())
+        .collect(Collectors.toList());
   }
 }
--- shesmu-pluginapi/src/main/java/ca/on/oicr/gsi/shesmu/plugin/cache/SimpleRecord.java
+++ shesmu-pluginapi/src/main/java/ca/on/oicr/gsi/shesmu/plugin/cache/SimpleRecord.java
@@ -1,6 +1,5 @@
 package ca.on.oicr.gsi.shesmu.plugin.cache;
 
-import java.time.Duration;
 import java.time.Instant;
 import java.util.Optional;
 
@@ -9,54 +8,28 @@ import java.util.Optional;
  *
  * <p>If the value could not be fetched, the previous value will be used instead.
  */
-public final class SimpleRecord<V> implements Record<Optional<V>> {
+public final class SimpleRecord<V> extends BaseRecord<Optional<V>, Optional<V>> {
 
-  private Instant fetchTime = Instant.EPOCH;
   private final Updater<Optional<V>> fetcher;
-  private boolean initialState = true;
-  private final Owner owner;
-  private Optional<V> value;
 
   public SimpleRecord(Owner owner, Updater<Optional<V>> fetcher) {
-    this.owner = owner;
+    super(owner, Optional.empty());
     this.fetcher = fetcher;
-    this.value = Optional.empty();
   }
 
   @Override
-  public int collectionSize() {
+  protected int collectionSize(Optional<V> value) {
     return value.isPresent() ? 1 : 0;
   }
 
   @Override
-  public void invalidate() {
-    fetchTime = Instant.EPOCH;
+  protected Optional<V> unpack(Optional<V> state) {
+    return state;
   }
 
   @Override
-  public Instant lastUpdate() {
-    return fetchTime;
-  }
-
-  @Override
-  public synchronized Optional<V> refresh() {
-    final Instant now = Instant.now();
-    if (Duration.between(fetchTime, now).toMinutes() > owner.ttl()) {
-      try (AutoCloseable timer = refreshLatency.start(owner.name())) {
-        final Optional<V> buffer = fetcher.update(fetchTime);
-        if (buffer.isPresent()) {
-          value = buffer;
-          fetchTime = Instant.now();
-          initialState = false;
-        }
-      } catch (final Exception e) {
-        e.printStackTrace();
-        staleRefreshError.labels(owner.name()).inc();
-      }
-    }
-    if (initialState) {
-      throw new InitialCachePopulationException();
-    }
-    return value;
+  protected Optional<V> update(Optional<V> oldstate, Instant fetchTime) throws Exception {
+    final Optional<V> buffer = fetcher.update(fetchTime);
+    return buffer.isPresent() ? buffer : null;
   }
 }
