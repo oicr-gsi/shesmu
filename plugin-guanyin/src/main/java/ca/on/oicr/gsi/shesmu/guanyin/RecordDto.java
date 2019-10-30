0ba88fd2dbf32b7bdf00a461f16e0deca2f863a4
Andre Masella <andre.masella@oicr.on.ca>
2019-10-30 17:59:18 -0400

    Create an input format for Guanyin records

--- plugin-guanyin/src/main/java/ca/on/oicr/gsi/shesmu/guanyin/RecordDto.java
+++ plugin-guanyin/src/main/java/ca/on/oicr/gsi/shesmu/guanyin/RecordDto.java
@@ -2,6 +2,8 @@ package ca.on.oicr.gsi.shesmu.guanyin;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.util.List;
 
 /**
  * Bean of record responses from Guanyin
@@ -10,10 +12,25 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RecordDto {
+  private List<String> filesIn;
   private boolean finished;
+  private String freshestInputDate;
   private String generated;
   private long id;
+  private boolean notificationDone;
+  private ObjectNode parameters;
   private long report;
+  private String reportPath;
+
+  @JsonProperty("files_in")
+  public List<String> getFilesIn() {
+    return filesIn;
+  }
+
+  @JsonProperty("freshest_input_date")
+  public String getFreshestInputDate() {
+    return freshestInputDate;
+  }
 
   @JsonProperty("date_generated")
   public String getGenerated() {
@@ -25,17 +42,39 @@ public class RecordDto {
     return id;
   }
 
+  public ObjectNode getParameters() {
+    return parameters;
+  }
+
   @JsonProperty("report_id")
   public long getReport() {
     return report;
+  }
+
+  @JsonProperty("report_path")
+  public String getReportPath() {
+    return reportPath;
   }
 
   public boolean isFinished() {
     return finished;
   }
 
+  @JsonProperty("notification_done")
+  public boolean isNotificationDone() {
+    return notificationDone;
+  }
+
+  public void setFilesIn(List<String> filesIn) {
+    this.filesIn = filesIn;
+  }
+
   public void setFinished(boolean finished) {
     this.finished = finished;
+  }
+
+  public void setFreshestInputDate(String freshestInputDate) {
+    this.freshestInputDate = freshestInputDate;
   }
 
   public void setGenerated(String generated) {
@@ -46,7 +85,19 @@ public class RecordDto {
     this.id = id;
   }
 
+  public void setNotificationDone(boolean notificationDone) {
+    this.notificationDone = notificationDone;
+  }
+
+  public void setParameters(ObjectNode parameters) {
+    this.parameters = parameters;
+  }
+
   public void setReport(long report) {
     this.report = report;
+  }
+
+  public void setReportPath(String reportPath) {
+    this.reportPath = reportPath;
   }
 }
