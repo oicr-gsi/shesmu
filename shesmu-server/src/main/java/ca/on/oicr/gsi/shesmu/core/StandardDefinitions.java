4a4b486e879460d89bbc737577c527e955bd7ed3
Andre Masella <andre.masella@oicr.on.ca>
2019-11-28 12:04:58 -0500

    Add new functions
    * date_to_second
    * date_to_milli
    * json_object
    * path_replace_home
    * str_replace
    * str_to_path

--- shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/core/StandardDefinitions.java
+++ shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/core/StandardDefinitions.java
@@ -16,11 +16,17 @@ import ca.on.oicr.gsi.shesmu.core.signers.SignatureNames;
 import ca.on.oicr.gsi.shesmu.plugin.Tuple;
 import ca.on.oicr.gsi.shesmu.plugin.functions.FunctionParameter;
 import ca.on.oicr.gsi.shesmu.plugin.types.Imyhat;
+import ca.on.oicr.gsi.shesmu.runtime.RuntimeInterop;
+import ca.on.oicr.gsi.shesmu.runtime.RuntimeSupport;
 import ca.on.oicr.gsi.status.ConfigurationSection;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.PrintStream;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Set;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 import org.objectweb.asm.Type;
@@ -29,6 +35,27 @@ import org.objectweb.asm.commons.Method;
 
 /** The standard library of Shesmu */
 public final class StandardDefinitions implements DefinitionRepository {
+
+  @RuntimeInterop
+  public static JsonNode jsonObject(Set<Tuple> fields) {
+    final ObjectNode result = RuntimeSupport.MAPPER.createObjectNode();
+    for (final Tuple field : fields) {
+      result.put((String) field.get(0), (JsonNode) field.get(1));
+    }
+    return result;
+  }
+
+  @RuntimeInterop
+  public static Path replaceHome(Path original, Path homedir) {
+    if (original.getNameCount() == 0) {
+      return original;
+    }
+    final String start = original.getName(0).toString();
+    if (start.equals("$HOME") || start.equals("~")) {
+      return homedir.resolve(original.subpath(1, original.getNameCount()));
+    }
+    return original;
+  }
 
   /** Truncate a time stamp to midnight */
   public static Instant start_of_day(Instant input) {
@@ -45,29 +72,30 @@ public final class StandardDefinitions implements DefinitionRepository {
     return (Long) version.get(2) >= patch;
   }
 
+  private static final Type A_CHAR_SEQUENCE_TYPE = Type.getType(CharSequence.class);
   private static final Type A_INSTANT_TYPE = Type.getType(Instant.class);
-
+  private static final Type A_JSON_SIGNATURE_TYPE = Type.getType(JsonSigner.class);
   private static final Type A_NOTHING_ACTION_TYPE = Type.getType(NothingAction.class);
-
-  private static final ConstantDefinition[] CONSTANTS =
-      new ConstantDefinition[] {
-        ConstantDefinition.of(
-            "epoch", Instant.EPOCH, "The date at UNIX timestamp 0: 1970-01-01T00:00:00Z"),
-        new ConstantDefinition(
-            "now",
-            Imyhat.DATE,
-            "The current timestamp. This is fetched every time this constant is referenced, so now != now.",
-            null) {
-
-          @Override
-          protected void load(GeneratorAdapter methodGen) {
-            methodGen.invokeStatic(A_INSTANT_TYPE, METHOD_INSTANT__NOW);
-          }
-        }
-      };
+  private static final Type A_PATH_TYPE = Type.getType(Path.class);
+  private static final Type A_PATHS_TYPE = Type.getType(Paths.class);
+  private static final Type A_SHA1_SIGNATURE_TYPE = Type.getType(SHA1DigestSigner.class);
+  protected static final Type A_STRING_ARRAY_TYPE = Type.getType(String[].class);
+  protected static final Type A_STRING_TYPE = Type.getType(String.class);
   private static final Method DEFAULT_CTOR = new Method("<init>", Type.VOID_TYPE, new Type[] {});
   private static final FunctionDefinition[] FUNCTIONS =
       new FunctionDefinition[] {
+        FunctionDefinition.virtualMethod(
+            "date_to_seconds",
+            "getEpochSecond",
+            "Get the number of seconds since the UNIX epoch for this date.",
+            Imyhat.INTEGER,
+            new FunctionParameter("date", Imyhat.DATE)),
+        FunctionDefinition.virtualMethod(
+            "date_to_millis",
+            "toEpochMilli",
+            "Get the number of milliseconds since the UNIX epoch for this date.",
+            Imyhat.INTEGER,
+            new FunctionParameter("date", Imyhat.DATE)),
         FunctionDefinition.staticMethod(
             "is_infinite",
             Double.class,
@@ -89,6 +117,13 @@ public final class StandardDefinitions implements DefinitionRepository {
             "Rounds a date-time to the previous midnight.",
             Imyhat.DATE,
             new FunctionParameter("date", Imyhat.DATE)),
+        FunctionDefinition.staticMethod(
+            "json_object",
+            StandardDefinitions.class,
+            "jsonObject",
+            "Create a JSON object from fields.",
+            Imyhat.JSON,
+            new FunctionParameter("fields", Imyhat.tuple(Imyhat.STRING, Imyhat.JSON).asList())),
         FunctionDefinition.virtualMethod(
             "path_file",
             "getFileName",
@@ -114,6 +149,14 @@ public final class StandardDefinitions implements DefinitionRepository {
             Imyhat.PATH,
             new FunctionParameter("directory path", Imyhat.PATH),
             new FunctionParameter("path to relativize", Imyhat.PATH)),
+        FunctionDefinition.staticMethod(
+            "path_replace_home",
+            StandardDefinitions.class,
+            "replaceHome",
+            "Replace any path that starts with $HOME or ~ with the provided home directory",
+            Imyhat.PATH,
+            new FunctionParameter("path to change", Imyhat.PATH),
+            new FunctionParameter("home directory", Imyhat.PATH)),
         FunctionDefinition.staticMethod(
             "version_at_least",
             StandardDefinitions.class,
@@ -158,13 +201,13 @@ public final class StandardDefinitions implements DefinitionRepository {
           }
 
           @Override
-          public String name() {
-            return "str_len";
+          public Path filename() {
+            return null;
           }
 
           @Override
-          public Path filename() {
-            return null;
+          public String name() {
+            return "str_len";
           }
 
           @Override
@@ -175,7 +218,7 @@ public final class StandardDefinitions implements DefinitionRepository {
           @Override
           public void render(GeneratorAdapter methodGen) {
             methodGen.invokeVirtual(
-                Type.getType(String.class), new Method("length", Type.INT_TYPE, new Type[] {}));
+                A_STRING_TYPE, new Method("length", Type.INT_TYPE, new Type[] {}));
             methodGen.cast(Type.INT_TYPE, Type.LONG_TYPE);
           }
 
@@ -193,17 +236,103 @@ public final class StandardDefinitions implements DefinitionRepository {
 
           @Override
           public String description() {
-            return "Truncates a floating point number of an integer.";
-          }
-
-          @Override
-          public String name() {
-            return "to_int";
+            return "Replaces parts of a string.";
           }
 
           @Override
           public Path filename() {
             return null;
+          }
+
+          @Override
+          public String name() {
+            return "str_replace";
+          }
+
+          @Override
+          public Stream<FunctionParameter> parameters() {
+            return Stream.of(
+                new FunctionParameter("haystack", Imyhat.STRING),
+                new FunctionParameter("needle", Imyhat.STRING),
+                new FunctionParameter("replacement", Imyhat.STRING));
+          }
+
+          @Override
+          public void render(GeneratorAdapter methodGen) {
+            methodGen.invokeVirtual(
+                A_STRING_TYPE,
+                new Method(
+                    "replace",
+                    A_STRING_TYPE,
+                    new Type[] {A_CHAR_SEQUENCE_TYPE, A_CHAR_SEQUENCE_TYPE}));
+          }
+
+          @Override
+          public void renderStart(GeneratorAdapter methodGen) {
+            // None required.
+          }
+
+          @Override
+          public Imyhat returnType() {
+            return Imyhat.STRING;
+          }
+        },
+        new FunctionDefinition() {
+
+          @Override
+          public String description() {
+            return "Converts a string to a path.";
+          }
+
+          @Override
+          public Path filename() {
+            return null;
+          }
+
+          @Override
+          public String name() {
+            return "str_to_path";
+          }
+
+          @Override
+          public Stream<FunctionParameter> parameters() {
+            return Stream.of(new FunctionParameter("str", Imyhat.STRING));
+          }
+
+          @Override
+          public void render(GeneratorAdapter methodGen) {
+            methodGen.push(0);
+            methodGen.newArray(A_STRING_TYPE);
+            methodGen.invokeStatic(
+                A_PATHS_TYPE,
+                new Method("get", A_PATH_TYPE, new Type[] {A_STRING_TYPE, A_STRING_ARRAY_TYPE}));
+          }
+
+          @Override
+          public void renderStart(GeneratorAdapter methodGen) {
+            // None required.
+          }
+
+          @Override
+          public Imyhat returnType() {
+            return Imyhat.PATH;
+          }
+        },
+        new FunctionDefinition() {
+
+          @Override
+          public String description() {
+            return "Truncates a floating point number of an integer.";
+          }
+
+          @Override
+          public Path filename() {
+            return null;
+          }
+
+          @Override
+          public String name() {
+            return "to_int";
           }
 
           @Override
@@ -234,13 +363,13 @@ public final class StandardDefinitions implements DefinitionRepository {
           }
 
           @Override
-          public String name() {
-            return "to_float";
+          public Path filename() {
+            return null;
           }
 
           @Override
-          public Path filename() {
-            return null;
+          public String name() {
+            return "to_float";
           }
 
           @Override
@@ -266,9 +395,22 @@ public final class StandardDefinitions implements DefinitionRepository {
       };
   private static final Method METHOD_INSTANT__NOW =
       new Method("now", A_INSTANT_TYPE, new Type[] {});
+  private static final ConstantDefinition[] CONSTANTS =
+      new ConstantDefinition[] {
+        ConstantDefinition.of(
+            "epoch", Instant.EPOCH, "The date at UNIX timestamp 0: 1970-01-01T00:00:00Z"),
+        new ConstantDefinition(
+            "now",
+            Imyhat.DATE,
+            "The current timestamp. This is fetched every time this constant is referenced, so now != now.",
+            null) {
 
-  protected static final Type A_STRING_TYPE = Type.getType(String.class);
-
+          @Override
+          protected void load(GeneratorAdapter methodGen) {
+            methodGen.invokeStatic(A_INSTANT_TYPE, METHOD_INSTANT__NOW);
+          }
+        }
+      };
   private static final ActionDefinition NOTHING_ACTION =
       new ActionDefinition(
           "nothing",
@@ -310,8 +452,6 @@ public final class StandardDefinitions implements DefinitionRepository {
           methodGen.invokeConstructor(A_NOTHING_ACTION_TYPE, DEFAULT_CTOR);
         }
       };
-  private static final Type A_JSON_SIGNATURE_TYPE = Type.getType(JsonSigner.class);
-  private static final Type A_SHA1_SIGNATURE_TYPE = Type.getType(SHA1DigestSigner.class);
   private static final SignatureDefinition[] SIGNATURES =
       new SignatureDefinition[] {
         new SignatureCount(),
@@ -357,11 +497,6 @@ public final class StandardDefinitions implements DefinitionRepository {
   }
 
   @Override
-  public Stream<RefillerDefinition> refillers() {
-    return Stream.empty();
-  }
-
-  @Override
   public Stream<FunctionDefinition> functions() {
     return Stream.of(FUNCTIONS);
   }
@@ -372,13 +507,18 @@ public final class StandardDefinitions implements DefinitionRepository {
   }
 
   @Override
-  public void writeJavaScriptRenderer(PrintStream writer) {
-    writer.print(
-        "actionRender.set('nothing', a => [title(a, 'Nothing'), text(`Value: ${a.value}`)]);");
+  public Stream<RefillerDefinition> refillers() {
+    return Stream.empty();
   }
 
   @Override
   public Stream<SignatureDefinition> signatures() {
     return Stream.of(SIGNATURES);
+  }
+
+  @Override
+  public void writeJavaScriptRenderer(PrintStream writer) {
+    writer.print(
+        "actionRender.set('nothing', a => [title(a, 'Nothing'), text(`Value: ${a.value}`)]);");
   }
 }
