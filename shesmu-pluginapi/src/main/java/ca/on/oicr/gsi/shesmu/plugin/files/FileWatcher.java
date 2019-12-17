d79a0a47c8ca619545725e2247a203309c22a084
Andre Masella <andre.masella@oicr.on.ca>
2019-12-17 14:51:19 -0500

    Monitor how long updating files from disk takes

--- shesmu-pluginapi/src/main/java/ca/on/oicr/gsi/shesmu/plugin/files/FileWatcher.java
+++ shesmu-pluginapi/src/main/java/ca/on/oicr/gsi/shesmu/plugin/files/FileWatcher.java
@@ -1,6 +1,5 @@
 package ca.on.oicr.gsi.shesmu.plugin.files;
 
-import ca.on.oicr.gsi.Pair;
 import io.prometheus.client.Gauge;
 import java.io.File;
 import java.io.IOException;
@@ -12,6 +11,7 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -20,12 +20,42 @@ import java.util.stream.Stream;
 /** Wrapper around Java's file watching to provide a better interface */
 public abstract class FileWatcher {
   private static final class RealFileWatcher extends FileWatcher {
+    private static final class RetryProcess {
+      private final WatchedFileListener listener;
+      private final Path path;
+      private final Instant retryTime;
+
+      private RetryProcess(Instant retryTime, WatchedFileListener listener, Path path) {
+        this.retryTime = retryTime;
+        this.listener = listener;
+        this.path = path;
+      }
+
+      public String filename() {
+        return path.toString();
+      }
+
+      public Instant time() {
+        return retryTime;
+      }
+
+      public void update(Consumer<RetryProcess> next, Instant now) {
+        listener
+            .update()
+            .ifPresent(
+                retryTimeout ->
+                    next.accept(
+                        new RetryProcess(
+                            now.plus(retryTimeout, ChronoUnit.MINUTES), listener, path)));
+      }
+    }
+
     private final Map<Path, List<WatchedFileListener>> active = new ConcurrentHashMap<>();
     private final Map<String, List<Function<Path, WatchedFileListener>>> ctors =
         new ConcurrentHashMap<>();
     private final List<Path> directories;
-    private final PriorityBlockingQueue<Pair<Instant, WatchedFileListener>> retry =
-        new PriorityBlockingQueue<>(200, Comparator.comparing(Pair::first));
+    private final PriorityBlockingQueue<RetryProcess> retry =
+        new PriorityBlockingQueue<>(200, Comparator.comparing(RetryProcess::time));
     private volatile boolean running = true;
     private final Thread watchThread = new Thread(this::run, "file-watcher");
 
@@ -97,7 +127,7 @@ public abstract class FileWatcher {
                     }
                     fileHolder.add(file);
                     updateTime.labels(path.toString()).setToCurrentTime();
-                    start(file);
+                    start(file, path);
                   });
         } catch (final IOException e) {
           e.printStackTrace();
@@ -119,7 +149,7 @@ public abstract class FileWatcher {
             final Instant now = Instant.now();
             final Optional<Long> timeout =
                 Optional.ofNullable(retry.peek())
-                    .map(p -> Duration.between(now, p.first()).toMillis());
+                    .map(p -> Duration.between(now, p.time()).toMillis());
             final WatchKey wk =
                 timeout.isPresent()
                     ? timeout.get() < 0
@@ -128,21 +158,15 @@ public abstract class FileWatcher {
                     : watchService.take();
             if (wk == null) {
               System.out.println("Timeout occurred. Reloading stale files.");
-              final List<Pair<Instant, WatchedFileListener>> retryOutput = new ArrayList<>();
+              final List<RetryProcess> retryOutput = new ArrayList<>();
               while (true) {
-                final Pair<Instant, WatchedFileListener> current = retry.poll();
+                final RetryProcess current = retry.poll();
                 if (current == null) break;
-                if (Duration.between(now, current.first()).toMillis() <= 0) {
-                  try {
-                    current
-                        .second()
-                        .update()
-                        .ifPresent(
-                            retryTimeout ->
-                                retryOutput.add(
-                                    new Pair<>(
-                                        now.plus(retryTimeout, ChronoUnit.MINUTES),
-                                        current.second())));
+                if (Duration.between(now, current.time()).toMillis() <= 0) {
+                  try (AutoCloseable timer =
+                      fileUpdateDuration.labels(current.filename()).startTimer()) {
+                    current.update(retryOutput::add, now);
+                    ;
                   } catch (Exception e) {
                     e.printStackTrace();
                   }
@@ -169,14 +193,14 @@ public abstract class FileWatcher {
                           .filter(entry -> fileName.endsWith(entry.getKey()))
                           .flatMap(entry -> entry.getValue().stream())
                           .map(ctor -> ctor.apply(path))
-                          .peek(this::start)
+                          .peek(listener -> start(listener, path))
                           .collect(Collectors.toList()));
                   updateTime.labels(path.toString()).setToCurrentTime();
                 } else if (event.kind() == StandardWatchEventKinds.ENTRY_DELETE) {
                   System.out.printf("File %s deleted.\n", path.toString());
                   final List<WatchedFileListener> listeners = active.remove(path);
                   if (listeners != null) {
-                    retry.removeIf(e -> listeners.contains(e.second()));
+                    retry.removeIf(e -> listeners.contains(e.listener));
                     listeners.forEach(WatchedFileListener::stop);
                     updateTime.labels(path.toString()).setToCurrentTime();
                   }
@@ -184,19 +208,21 @@ public abstract class FileWatcher {
                   System.out.printf("File %s updated.\n", path.toString());
                   final List<WatchedFileListener> listeners = active.get(path);
                   if (listeners != null) {
-                    retry.removeIf(e -> listeners.contains(e.second()));
+                    retry.removeIf(e -> listeners.contains(e.listener));
                     listeners
                         .stream()
-                        .<Optional<Pair<Instant, WatchedFileListener>>>map(
+                        .<Optional<RetryProcess>>map(
                             listener -> {
-                              try {
+                              try (AutoCloseable timer =
+                                  fileUpdateDuration.labels(path.toString()).startTimer()) {
                                 return listener
                                     .update()
                                     .map(
                                         retryTimeout ->
-                                            new Pair<>(
+                                            new RetryProcess(
                                                 now.plus(retryTimeout, ChronoUnit.MINUTES),
-                                                listener));
+                                                listener,
+                                                path));
                               } catch (Exception e) {
                                 e.printStackTrace();
                                 return Optional.empty();
@@ -220,12 +246,14 @@ public abstract class FileWatcher {
       }
     }
 
-    private void start(WatchedFileListener file) {
+    private void start(WatchedFileListener file, Path path) {
       file.start();
       file.update()
           .ifPresent(
               timeout ->
-                  retry.offer(new Pair<>(Instant.now().plus(timeout, ChronoUnit.MINUTES), file)));
+                  retry.offer(
+                      new RetryProcess(
+                          Instant.now().plus(timeout, ChronoUnit.MINUTES), file, path)));
     }
   }
 
@@ -248,6 +276,10 @@ public abstract class FileWatcher {
                       // Do nothing
                     }
                   });
+  private static final Gauge fileUpdateDuration =
+      Gauge.build("shesmu_file_update_duration", "Number of seconds to update a Shesmu file")
+          .labelNames("filename")
+          .register();
   private static final Gauge updateTime =
       Gauge.build(
               "shesmu_auto_update_timestamp",
