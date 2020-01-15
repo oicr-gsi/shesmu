cfeaf81c273ed8eb9ea9430b55148011046e4e7a
Andre Masella <andre.masella@oicr.on.ca>
2020-01-15 15:01:05 -0500

    Add WDL for type parsing page

--- shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/server/TypeParseResponse.java
+++ shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/server/TypeParseResponse.java
@@ -1,14 +1,17 @@
 package ca.on.oicr.gsi.shesmu.server;
 
 import ca.on.oicr.gsi.shesmu.plugin.types.Imyhat;
+import ca.on.oicr.gsi.shesmu.plugin.wdl.WdlInputType;
 
 public class TypeParseResponse {
   private String descriptor;
   private String humanName;
+  private String wdlType;
 
   public TypeParseResponse(Imyhat input) {
     humanName = input.name();
     descriptor = input.descriptor();
+    wdlType = input.apply(WdlInputType.TO_WDL_TYPE);
   }
 
   public String getDescriptor() {
@@ -19,11 +22,19 @@ public class TypeParseResponse {
     return humanName;
   }
 
+  public String getWdlType() {
+    return wdlType;
+  }
+
   public void setDescriptor(String descriptor) {
     this.descriptor = descriptor;
   }
 
   public void setHumanName(String humanName) {
     this.humanName = humanName;
+  }
+
+  public void setWdlType(String wdlType) {
+    this.wdlType = wdlType;
   }
 }
