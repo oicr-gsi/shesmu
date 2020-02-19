5d96060b379a6352c98c47c26df625245cba99ad
Andre Masella <andre.masella@oicr.on.ca>
2020-02-19 17:44:18 -0500

    Fix cache exception

--- shesmu-pluginapi/src/main/java/ca/on/oicr/gsi/shesmu/plugin/cache/InitialCachePopulationException.java
+++ shesmu-pluginapi/src/main/java/ca/on/oicr/gsi/shesmu/plugin/cache/InitialCachePopulationException.java
@@ -2,6 +2,6 @@ package ca.on.oicr.gsi.shesmu.plugin.cache;
 
 public class InitialCachePopulationException extends RuntimeException {
   public InitialCachePopulationException(String cache) {
-    super(cache);
+    super("Failed to populate cache: " + cache);
   }
 }
