b21beca6df680a0934fdb441b28af7615fd0620b
Andre Masella <andre.masella@oicr.on.ca>
2020-02-26 10:36:01 -0500

    Combine JSON packing and unpacking
    This decided which type to use before type variable resolution was applied. This mean the following:
    
    ```
    TypeAlias foo json;
    
    x = 3;
    
    ...
      x As json == x As foo
    ```
    
    would produce a type error because `x As json` would produce the type `json`,
    since it thinks is packing, and `x As foo` would produce the type `json?`,
    since it thinks its unpacking.

--- shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/compiler/ExpressionNodeJsonConvert.java
+++ shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/compiler/ExpressionNodeJsonConvert.java
@@ -1,8 +1,11 @@
 package ca.on.oicr.gsi.shesmu.compiler;
 
+import static ca.on.oicr.gsi.shesmu.compiler.TypeUtils.TO_ASM;
+
 import ca.on.oicr.gsi.shesmu.plugin.types.Imyhat;
 import ca.on.oicr.gsi.shesmu.plugin.types.ImyhatTransformer;
 import ca.on.oicr.gsi.shesmu.runtime.JsonConverter;
+import ca.on.oicr.gsi.shesmu.runtime.JsonWrapper;
 import com.fasterxml.jackson.databind.JsonNode;
 import java.nio.file.Path;
 import java.util.Optional;
@@ -12,21 +15,27 @@ import java.util.function.Predicate;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.commons.Method;
 
-public class ExpressionNodeJsonUnpack extends ExpressionNode {
+public class ExpressionNodeJsonConvert extends ExpressionNode {
   private static final Type A_IMYHAT_TYPE = Type.getType(Imyhat.class);
   private static final Type A_JSON_CONVERTER_TYPE = Type.getType(JsonConverter.class);
   private static final Type A_JSON_NODE_TYPE = Type.getType(JsonNode.class);
+  private static final Type A_JSON_WRAPPER_TYPE = Type.getType(JsonWrapper.class);
   private static final Type A_OPTIONAL_TYPE = Type.getType(Optional.class);
   private static final Method IMYHAT__APPLY =
       new Method(
           "apply", Type.getType(Object.class), new Type[] {Type.getType(ImyhatTransformer.class)});
+  private static final Method JSON_CONVERTER__CONVERT =
+      new Method(
+          "convert",
+          Type.getType(JsonNode.class),
+          new Type[] {Type.getType(Imyhat.class), Type.getType(Object.class)});
   private static final Method JSON_CONVERTER__CTOR =
       new Method("<init>", Type.VOID_TYPE, new Type[] {A_JSON_NODE_TYPE});
   private final ExpressionNode expression;
   private final ImyhatNode imyhatNode;
   private Imyhat type = Imyhat.BAD;
 
-  public ExpressionNodeJsonUnpack(
+  public ExpressionNodeJsonConvert(
       int line, int column, ExpressionNode expression, ImyhatNode imyhatNode) {
     super(line, column);
     this.expression = expression;
@@ -45,13 +54,20 @@ public class ExpressionNodeJsonUnpack extends ExpressionNode {
 
   @Override
   public void render(Renderer renderer) {
-    renderer.loadImyhat(type.descriptor());
-    renderer.methodGen().newInstance(A_JSON_CONVERTER_TYPE);
-    renderer.methodGen().dup();
-    expression.render(renderer);
-    renderer.methodGen().invokeConstructor(A_JSON_CONVERTER_TYPE, JSON_CONVERTER__CTOR);
-    renderer.methodGen().invokeVirtual(A_IMYHAT_TYPE, IMYHAT__APPLY);
-    renderer.methodGen().checkCast(A_OPTIONAL_TYPE);
+    if (type.isSame(Imyhat.JSON)) {
+      renderer.loadImyhat(expression.type().descriptor());
+      expression.render(renderer);
+      renderer.methodGen().valueOf(expression.type().apply(TO_ASM));
+      renderer.methodGen().invokeStatic(A_JSON_WRAPPER_TYPE, JSON_CONVERTER__CONVERT);
+    } else {
+      renderer.loadImyhat(type.descriptor());
+      renderer.methodGen().newInstance(A_JSON_CONVERTER_TYPE);
+      renderer.methodGen().dup();
+      expression.render(renderer);
+      renderer.methodGen().invokeConstructor(A_JSON_CONVERTER_TYPE, JSON_CONVERTER__CTOR);
+      renderer.methodGen().invokeVirtual(A_IMYHAT_TYPE, IMYHAT__APPLY);
+      renderer.methodGen().checkCast(A_OPTIONAL_TYPE);
+    }
   }
 
   @Override
@@ -68,13 +84,16 @@ public class ExpressionNodeJsonUnpack extends ExpressionNode {
 
   @Override
   public Imyhat type() {
-    return type.asOptional();
+    return type.isSame(Imyhat.JSON) ? Imyhat.JSON : type.asOptional();
   }
 
   @Override
   public boolean typeCheck(Consumer<String> errorHandler) {
     if (expression.typeCheck(errorHandler)) {
-      if (expression.type().isSame(Imyhat.JSON)) {
+      // We are handling two cases: convert things to JSON (in which case, we don't care what the
+      // input type is) or convert from JSON (in which case, the input type must be JSON and we
+      // don't care about the output type)
+      if (type.isSame(Imyhat.JSON) || expression.type().isSame(Imyhat.JSON)) {
         return true;
       }
       expression.typeError("json", expression.type(), errorHandler);
