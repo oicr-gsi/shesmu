ded12cb16315c3bd1406f87a3c44a73a2e91b23b
Andre Masella <andre.masella@oicr.on.ca>
2020-03-02 16:08:36 -0500

    Allow more arguments to custom groupers

--- shesmu-pluginapi/src/main/java/ca/on/oicr/gsi/shesmu/plugin/grouper/GrouperDefinition.java
+++ shesmu-pluginapi/src/main/java/ca/on/oicr/gsi/shesmu/plugin/grouper/GrouperDefinition.java
@@ -31,12 +31,14 @@ public abstract class GrouperDefinition {
   private static final MethodHandle MH_FUNCTION__APPLY;
   private static final MethodHandle MH_PACK3__PACK;
   private static final MethodHandle MH_PACK4__PACK;
+  private static final MethodHandle MH_PACK5__PACK;
 
   static {
     MethodHandle mh_function__apply;
     MethodHandle mh_bifunction_apply;
     MethodHandle mh_pack3__pack;
     MethodHandle mh_pack4__pack;
+    MethodHandle mh_pack5__pack;
 
     try {
       MethodHandles.Lookup lookup = MethodHandles.publicLookup();
@@ -59,6 +61,17 @@ public abstract class GrouperDefinition {
               "pack",
               MethodType.methodType(
                   Object.class, Object.class, Object.class, Object.class, Object.class));
+      mh_pack5__pack =
+          lookup.findVirtual(
+              TypeGuarantee.Pack5.class,
+              "pack",
+              MethodType.methodType(
+                  Object.class,
+                  Object.class,
+                  Object.class,
+                  Object.class,
+                  Object.class,
+                  Object.class));
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
@@ -66,6 +79,7 @@ public abstract class GrouperDefinition {
     MH_BIFUNCTION_APPLY = mh_bifunction_apply;
     MH_PACK3__PACK = mh_pack3__pack;
     MH_PACK4__PACK = mh_pack4__pack;
+    MH_PACK5__PACK = mh_pack5__pack;
   }
 
   private final List<GrouperParameter<?, ?>> inputs;
@@ -178,7 +192,9 @@ public abstract class GrouperDefinition {
    * @param <I> the type of the input rows; this is controlled by Shesmu
    * @param <O> the type of the ouput rows; this is controlled by Shesmu
    * @param <G> the type of the grouping operation
-   * @param <T> the type of the argument to the grouping operation
+   * @param <T> the type of the first argument to the grouping operation
+   * @param <S> the type of the second argument to the grouping operation
+   * @param <U> the type of the third argument to the grouping operation
    * @param <C> the type of the grouped collector
    */
   public <I, O, G extends Grouper<I, O>, T, S, U, C> GrouperDefinition(
@@ -204,6 +220,55 @@ public abstract class GrouperDefinition {
               }
             }));
     this.inputs = Arrays.asList(parameter1, parameter2, parameter3);
+    this.outputs = outputs;
+  }
+
+  /**
+   * Define a new grouper that takes three arguments
+   *
+   * @param name the name of the grouper that should be used in the olive; it must be a valid Shesmu
+   *     identifier
+   * @param parameter1 the first input the olives must provide
+   * @param parameter2 the second input the olives must provide
+   * @param parameter3 the third input the olives must provide
+   * @param parameter4 the fourth input the olives must provide
+   * @param outputs the collector format the olive will provided for exporting the extra subgroup
+   *     information back to the olive
+   * @param factory a function that creates a new grouper
+   * @param <I> the type of the input rows; this is controlled by Shesmu
+   * @param <O> the type of the ouput rows; this is controlled by Shesmu
+   * @param <G> the type of the grouping operation
+   * @param <T> the type of the first argument to the grouping operation
+   * @param <S> the type of the second argument to the grouping operation
+   * @param <U> the type of the third argument to the grouping operation
+   * @param <V> the type of the fourth argument to the grouping operation
+   * @param <C> the type of the grouped collector
+   */
+  public <I, O, G extends Grouper<I, O>, T, S, U, V, C> GrouperDefinition(
+      String name,
+      GrouperParameter<I, T> parameter1,
+      GrouperParameter<I, S> parameter2,
+      GrouperParameter<I, U> parameter3,
+      GrouperParameter<I, V> parameter4,
+      GrouperOutputs<I, O, C> outputs,
+      TypeGuarantee.Pack5<? super T, ? super S, ? super U, ? super V, ? super C, G> factory) {
+    this.name = name;
+    HANDLES.put(
+        name,
+        MH_PACK5__PACK.bindTo(
+            new TypeGuarantee.Pack5<Object, Object, Object, Object, C, Grouper<I, O>>() {
+              @Override
+              public Grouper<I, O> pack(
+                  Object first, Object second, Object third, Object fourth, C outputGenerator) {
+                return factory.pack(
+                    parameter1.unpack(first),
+                    parameter2.unpack(second),
+                    parameter3.unpack(third),
+                    parameter4.unpack(fourth),
+                    outputGenerator);
+              }
+            }));
+    this.inputs = Arrays.asList(parameter1, parameter2, parameter3, parameter4);
     this.outputs = outputs;
   }
 
