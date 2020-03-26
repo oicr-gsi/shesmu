e622dab72d209b2b0fa41df820a376709761fe7b
Andre Masella <andre@masella.name>
2020-03-26 14:33:54 -0400

    Fix layout of alert breakdown table in Firefox

--- shesmu-server/src/main/resources/ca/on/oicr/gsi/shesmu/shesmu.js
+++ shesmu-server/src/main/resources/ca/on/oicr/gsi/shesmu/shesmu.js
@@ -4453,8 +4453,10 @@ function showAlertNavigator(
             if (activeBreakdown == row) {
               row.style.backgroundColor = "inherit";
               activeBreakdown = null;
+              table.style.width = null;
               return;
             }
+            table.style.width = "auto";
             if (activeBreakdown) {
               activeBreakdown.style.backgroundColor = "inherit";
             }
