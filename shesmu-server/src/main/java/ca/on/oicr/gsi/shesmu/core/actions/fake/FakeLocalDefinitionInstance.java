45a21cb4d59c20ae995a67d6fb3574e4f44c870d
Andre Masella <andre@masella.name>
2020-07-10 17:31:35 -0400

    Create local fake actions repository

---
+++ shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/core/actions/fake/FakeLocalDefinitionInstance.java
@@ -0,0 +1,32 @@
+package ca.on.oicr.gsi.shesmu.core.actions.fake;
+
+import ca.on.oicr.gsi.shesmu.plugin.Definer;
+import ca.on.oicr.gsi.shesmu.plugin.PluginFileType;
+import java.io.PrintStream;
+import java.lang.invoke.MethodHandles;
+import java.nio.file.Path;
+import org.kohsuke.MetaInfServices;
+
+/**
+ * Create actions that work on action definitions downloaded from an existing Shesmu instance, but
+ * do nothing when executed
+ *
+ * <p>This is for preparation of development servers
+ */
+@MetaInfServices(PluginFileType.class)
+public class FakeLocalDefinitionInstance extends PluginFileType<LocalFile> {
+
+  public FakeLocalDefinitionInstance() {
+    super(MethodHandles.lookup(), LocalFile.class, ".fakeactiondefs", "fake");
+  }
+
+  @Override
+  public void writeJavaScriptRenderer(PrintStream writer) {
+    // Covered by its sibling
+  }
+
+  @Override
+  public LocalFile create(Path filePath, String instanceName, Definer<LocalFile> definer) {
+    return new LocalFile(filePath, instanceName, definer);
+  }
+}
