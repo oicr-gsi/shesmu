42629064e5e07e0cd9a4bb718716912cb4dbc848
Andre Masella <andre@masella.name>
2020-12-24 21:56:39 -0500

    Create a Grafana plugin to access Shesmu action counts
    This adds a Grafana plugin to fetch Shesmu action counts using advanced action
    queries. It cannot report a time series, since Shesmu does not keep that
    information, but it return the current count for actios matching the query.

---
+++ shesmu-grafana-datasource/src/QueryEditor.tsx
@@ -0,0 +1,35 @@
+import defaults from 'lodash/defaults';
+
+import React, { ChangeEvent, PureComponent } from 'react';
+import { LegacyForms } from '@grafana/ui';
+import { QueryEditorProps } from '@grafana/data';
+import { DataSource } from './DataSource';
+import { defaultQuery, ShesmuDataSourceOptions, ActionQuery } from './types';
+
+const { FormField } = LegacyForms;
+
+type Props = QueryEditorProps<DataSource, ActionQuery, ShesmuDataSourceOptions>;
+
+export class QueryEditor extends PureComponent<Props> {
+  onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onChange, query } = this.props;
+    onChange({ ...query, queryText: event.target.value });
+  };
+
+  render() {
+    const query = defaults(this.props.query, defaultQuery);
+    const { queryText } = query;
+
+    return (
+      <div className="gf-form">
+        <FormField
+          labelWidth={8}
+          value={queryText || ''}
+          onChange={this.onQueryTextChange}
+          label="Query Text"
+          tooltip="The action query to use. Consult the Shesmu actions dashboard in advanced mode for syntax."
+        />
+      </div>
+    );
+  }
+}
