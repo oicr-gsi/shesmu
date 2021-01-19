f12407fe872f1d16f5cc2902aa0323d212fad181
Andre Masella <andre@masella.name>
2021-01-19 14:34:54 -0500

    Fix bug preventing _Pause Script_ button from showing
    It needs to make a request to the server to know its own state and there was a
    check that only made the request happen when it was trying to change its state.

--- shesmu-server-ui/src/olive.ts
+++ shesmu-server-ui/src/olive.ts
@@ -412,7 +412,7 @@ export function initialiseOliveDash(
       "pausefile",
       pauseFileModel,
       (reference: OliveReference, desired: boolean | null) =>
-        reference && desired !== null
+        reference
           ? {
             file: reference.script.file,
             line: null,
