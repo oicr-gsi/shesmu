4c56bb8d8b4d00c980146601628483a84f76fdcf
Andre Masella <andre@masella.name>
2021-03-25 20:05:41 -0400

    Allow using custom lookup environment for input formats
    Shesmu uses reflection and annotation to allow the compiler to bind to custom
    data formats. When in a module, either the data format needs to be public or
    the data format needs to provide a method handle lookup context with private
    visibility. This allows the data format author to decide and moves some of the
    internal data formats to private access.

--- shesmu-pluginapi/src/main/java/ca/on/oicr/gsi/shesmu/plugin/input/InputFormat.java
+++ shesmu-pluginapi/src/main/java/ca/on/oicr/gsi/shesmu/plugin/input/InputFormat.java
@@ -1,5 +1,8 @@
 package ca.on.oicr.gsi.shesmu.plugin.input;
 
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodHandles.Lookup;
+
 /**
  * Define a new input format
  *
@@ -7,16 +10,32 @@ package ca.on.oicr.gsi.shesmu.plugin.input;
  * a plugin
  */
 public class InputFormat {
+  private static final Lookup PUBLIC_LOOKUP = MethodHandles.publicLookup();
+  private final Lookup lookup;
   private final String name;
   private final Class<?> clazz;
 
   /**
    * Create a new input format
    *
+   * <p>This uses public lookup. If your class is not publically visible, use the other constructor
+   * and provide a better lookup.
+   *
    * @param name the name for the input format; this must be a valid Shesmu identifier
    * @param clazz the type of each row in the input format. This class must not be generic.
    */
   public InputFormat(String name, Class<?> clazz) {
+    this(name, clazz, PUBLIC_LOOKUP);
+  }
+  /**
+   * Create a new input format
+   *
+   * @param name the name for the input format; this must be a valid Shesmu identifier
+   * @param clazz the type of each row in the input format. This class must not be generic.
+   * @param lookup the reflection lookup context to investigate the class
+   */
+  public InputFormat(String name, Class<?> clazz, Lookup lookup) {
+    this.lookup = lookup;
     if (clazz.getTypeParameters().length > 0) {
       throw new IllegalArgumentException(
           String.format(
@@ -24,6 +43,10 @@ public class InputFormat {
     }
     this.name = name;
     this.clazz = clazz;
+  }
+
+  public Lookup lookup() {
+    return lookup;
   }
 
   public final String name() {
--- shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/core/input/shesmu/ShesmuIntrospectionFormat.java
+++ shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/core/input/shesmu/ShesmuIntrospectionFormat.java
@@ -1,12 +1,13 @@
 package ca.on.oicr.gsi.shesmu.core.input.shesmu;
 
 import ca.on.oicr.gsi.shesmu.plugin.input.InputFormat;
+import java.lang.invoke.MethodHandles;
 import org.kohsuke.MetaInfServices;
 
 @MetaInfServices(InputFormat.class)
-public class ShesmuIntrospectionFormat extends InputFormat {
+public final class ShesmuIntrospectionFormat extends InputFormat {
 
   public ShesmuIntrospectionFormat() {
-    super("shesmu", ShesmuIntrospectionValue.class);
+    super("shesmu", ShesmuIntrospectionValue.class, MethodHandles.lookup());
   }
 }
