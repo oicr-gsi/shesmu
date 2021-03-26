33a1ab13da9bcf733fa91439e0ea1e41dc6a6171
Andre Masella <andre@masella.name>
2021-03-26 07:43:57 -0400

    Create a new plugin to handle interval files
    This is intended as a replacement for RSConfig with only the functionality we
    actually use. See the manual for details, but it prepares the chromosomes list
    from the BED files and dispenses with the other file types RSConfig supports
    (_e.g._, GCBias) that we don't use.

---
+++ plugin-intervals/README.md
@@ -0,0 +1,31 @@
+# BED Interval File Plugin
+
+This plugin allows converting a directory of BED files for specific targeted sequencing
+panels into a lookup structure that contain path and chromosome information. A file ending
+`.intervalbed` described below points to a separate directory of BED files.
+
+    {
+       "directory": "panels",
+       "replacementPrefix": "/srv/panels"
+    }
+
+The `"directory"` property defines a path, relative to the configuration file to scan for BED files.
+The `"replacementPrefix"` defines a new prefix that should be used for paths given to the olives.
+That is, suppose this file lives in `/srv/shesmu/config`, then Shesmu will
+scan `/srv/shesmu/config/panels` for BED files and for a BED file `ALL.WG.hg38.bed`, it will be
+given to the olive as `/srv/panels/ALL.WG.hg38.bed`.
+
+The panels must have the format *panel*`.`*library-type*`.`*genome*`.bed`. The _genome_ can be any
+characters except `.`. The _library-type_ can be any uppercase characters. Multiple library types
+can be specified, separated by commas (_e.g._, `foo.EX,WG.hg19.bed`) The _panel_ can be any
+characters except `.`, but `ALL` is treated specially.
+
+When an olive performs a lookup, it must provide the panel, library type, and genome. First, an
+exact match is attempted (_e.g._, `::get("TS", "mm10", "IDT Mickey Mouse"` will look
+for `IDT Mickey Mouse.TS.mm10.bed`). If no exact match is found, then it will fall back on the `ALL`
+file, if available (_e.g._, `::get("WG", "mm10", "IDT Mickey Mouse"` will look
+for `IDT Mickey Mouse.WG.mm10.bed` first, then `ALL.WG.mm10.bed`).
+
+Sometimes a panel has multiple names or the name contains characters that should not be in files
+names (_.e.g_, `/`, `.`). In that case, a second file with the extension `.alias` can be provided
+with alternate panel names, one per line. Blank lines are ignored.
---
+++ plugin-intervals/src/main/java/ca/on/oicr/gsi/shesmu/intervals/IntervalFile.java
@@ -0,0 +1,117 @@
+package ca.on.oicr.gsi.shesmu.intervals;
+
+import ca.on.oicr.gsi.Pair;
+import ca.on.oicr.gsi.shesmu.plugin.Tuple;
+import ca.on.oicr.gsi.shesmu.plugin.functions.ShesmuMethod;
+import ca.on.oicr.gsi.shesmu.plugin.functions.ShesmuParameter;
+import ca.on.oicr.gsi.shesmu.plugin.json.JsonPluginFile;
+import ca.on.oicr.gsi.status.SectionRenderer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class IntervalFile extends JsonPluginFile<Configuration> {
+  private static final Pattern COMMA = Pattern.compile(",");
+  private static final Pattern FILE_NAME =
+      Pattern.compile("(?<panel>[^.]+)\\.(?<library>[A-Z]+(?:,[A-Z]+)*)\\.(?<genome>[^.]+)\\.bed");
+  private static final ObjectMapper MAPPER = new ObjectMapper();
+  private static final Pattern TAB = Pattern.compile("\\t");
+  private Map<IntervalKey, Tuple> files = Map.of();
+
+  public IntervalFile(Path fileName, String instanceName) {
+    super(fileName, instanceName, MAPPER, Configuration.class);
+  }
+
+  @Override
+  public void configuration(SectionRenderer renderer) {
+    renderer.line("Records", files.size());
+  }
+
+  @ShesmuMethod(
+      description =
+          "Get the best available panel configuration. A fallback panel may be used if one is available.",
+      type = "qo2chromosomes$sinterval_file$p")
+  public Optional<Tuple> get(
+      @ShesmuParameter(description = "The library type (e.g., WG, TS)") String library,
+      @ShesmuParameter(description = "The target genome name (e.g., hg19, mm10)") String genome,
+      @ShesmuParameter(description = "The targeted sequencing kit (e.g., IDT xGen Pan-Cancer)")
+          String panel) {
+    return Optional.ofNullable(files.get(new IntervalKey(panel, library, genome)))
+        .or(() -> Optional.ofNullable(files.get(new IntervalKey("ALL", library, genome))));
+  }
+
+  private String readChromosomes(Path file) {
+    try (final var bedContents = Files.lines(file, StandardCharsets.US_ASCII)) {
+      return bedContents.map(l -> TAB.split(l)[0]).distinct().collect(Collectors.joining(","));
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+  }
+
+  private List<String> readPanelNames(Path aliasFile, String panelName) {
+    try (final var panelsContents =
+        Stream.concat(
+            Stream.of(panelName),
+            Files.exists(aliasFile)
+                ? Files.lines(aliasFile, StandardCharsets.US_ASCII).filter(s -> !s.isBlank())
+                : Stream.empty())) {
+      return panelsContents.collect(Collectors.toList());
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+  }
+
+  @Override
+  protected Optional<Integer> update(Configuration value) {
+    final var root = fileName().resolveSibling(value.getDirectory());
+    try (final var files = Files.walk(root, 1)) {
+      this.files =
+          files
+              .flatMap(
+                  file -> {
+                    final var match = FILE_NAME.matcher(file.getFileName().toString());
+                    if (match.matches()) {
+                      final var bedFile =
+                          Path.of(value.getReplacementPrefix()).resolve(root.relativize(file));
+                      final var chromosomes = readChromosomes(file);
+                      final var panels =
+                          readPanelNames(
+                              root.resolve(
+                                  String.format(
+                                      "%s.%s.%s.alias",
+                                      match.group("panel"),
+                                      match.group("library"),
+                                      match.group("genome"))),
+                              match.group("panel"));
+
+                      return panels.stream()
+                          .flatMap(
+                              panel ->
+                                  COMMA
+                                      .splitAsStream(match.group("library"))
+                                      .map(
+                                          library ->
+                                              new Pair<>(
+                                                  new IntervalKey(
+                                                      panel, library, match.group("genome")),
+                                                  new Tuple(chromosomes, bedFile))));
+                    } else {
+                      return Stream.empty();
+                    }
+                  })
+              .collect(Collectors.toMap(Pair::first, Pair::second));
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+    return Optional.of(10);
+  }
+}
