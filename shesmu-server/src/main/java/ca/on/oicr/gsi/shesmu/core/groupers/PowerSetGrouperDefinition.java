f29bb17f25c16996c044044b443cb0c91cc98b90
Andre Masella <andre@masella.name>
2021-05-10 13:03:11 -0400

    Create a powerset grouper
    This grouper was used as a demonstration of how to write a grouper.

---
+++ shesmu-server/src/main/java/ca/on/oicr/gsi/shesmu/core/groupers/PowerSetGrouperDefinition.java
@@ -0,0 +1,17 @@
+package ca.on.oicr.gsi.shesmu.core.groupers;
+
+import ca.on.oicr.gsi.shesmu.plugin.grouper.GrouperDefinition;
+import ca.on.oicr.gsi.shesmu.plugin.grouper.GrouperOutputs;
+import org.kohsuke.MetaInfServices;
+
+@MetaInfServices
+public class PowerSetGrouperDefinition extends GrouperDefinition {
+  public PowerSetGrouperDefinition() {
+    super("powerset", GrouperOutputs.empty(), PowerSetGrouper::new);
+  }
+
+  @Override
+  public String description() {
+    return "Creates groups which are the powerset of each subgroup supplied.";
+  }
+}
