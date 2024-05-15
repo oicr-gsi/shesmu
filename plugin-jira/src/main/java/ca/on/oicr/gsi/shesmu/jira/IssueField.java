41ccd86914a53477f653fdc693fdaf1fb460b4bc
Alexis Varsava <alexis.varsava@oicr.on.ca>
2024-05-15 14:08:54 -0400

    make IssueField a record

--- plugin-jira/src/main/java/ca/on/oicr/gsi/shesmu/jira/IssueField.java
+++ plugin-jira/src/main/java/ca/on/oicr/gsi/shesmu/jira/IssueField.java
@@ -3,23 +3,4 @@ package ca.on.oicr.gsi.shesmu.jira;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public final class IssueField {
-  private boolean hasDefaultValue;
-  private boolean required;
-
-  public boolean isHasDefaultValue() {
-    return hasDefaultValue;
-  }
-
-  public boolean isRequired() {
-    return required;
-  }
-
-  public void setHasDefaultValue(boolean hasDefaultValue) {
-    this.hasDefaultValue = hasDefaultValue;
-  }
-
-  public void setRequired(boolean required) {
-    this.required = required;
-  }
-}
+public record IssueField(boolean hasDefaultValue, boolean required) {}
