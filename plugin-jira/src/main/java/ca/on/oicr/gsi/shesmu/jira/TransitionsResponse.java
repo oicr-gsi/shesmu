b4f16657e8aafeabc24dfe8466af96c8f4cd024f
Alexis Varsava <alexis.varsava@oicr.on.ca>
2024-05-15 10:24:30 -0400

    Make TransitionsResponse a record

--- plugin-jira/src/main/java/ca/on/oicr/gsi/shesmu/jira/TransitionsResponse.java
+++ plugin-jira/src/main/java/ca/on/oicr/gsi/shesmu/jira/TransitionsResponse.java
@@ -4,14 +4,4 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public final class TransitionsResponse {
-  private List<Transition> transitions;
-
-  public List<Transition> getTransitions() {
-    return transitions;
-  }
-
-  public void setTransitions(List<Transition> transitions) {
-    this.transitions = transitions;
-  }
-}
+public record TransitionsResponse(List<Transition> transitions) {}
