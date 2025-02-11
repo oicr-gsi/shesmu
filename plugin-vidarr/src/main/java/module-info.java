8259a691f688e16612a055ecdbfba37fafb062f0
grapigeau <gpigeau@oicr.on.ca>
2025-02-11 14:30:34 -0500

    add a new input format vidarr_provenance to vidarr plugin

--- plugin-vidarr/src/main/java/module-info.java
+++ plugin-vidarr/src/main/java/module-info.java
@@ -1,4 +1,6 @@
 import ca.on.oicr.gsi.shesmu.plugin.PluginFileType;
+import ca.on.oicr.gsi.shesmu.plugin.input.InputFormat;
+import ca.on.oicr.gsi.shesmu.vidarr.VidarrAnalysisFormatDefinition;
 import ca.on.oicr.gsi.shesmu.vidarr.VidarrPluginType;
 
 module ca.on.oicr.gsi.shesmu.plugin.vidarr {
@@ -6,10 +8,13 @@ module ca.on.oicr.gsi.shesmu.plugin.vidarr {
 
   requires ca.on.oicr.gsi.serverutils;
   requires ca.on.oicr.gsi.shesmu;
+  requires ca.on.oicr.gsi.shesmu.plugin.gsi.common;
   requires ca.on.oicr.gsi.vidarr.pluginapi;
   requires com.fasterxml.jackson.datatype.jsr310;
   requires java.net.http;
 
+  provides InputFormat with
+      VidarrAnalysisFormatDefinition;
   provides PluginFileType with
       VidarrPluginType;
 }
