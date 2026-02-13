f6f0951d9bc41ba4a0ca9915c55de003d48c6b43
Alexis Varsava <alexis.varsava@oicr.on.ca>
2026-02-13 15:47:12 -0500

    huh? i don't know what i was trying to do here

--- plugin-vidarr/src/main/java/ca/on/oicr/gsi/shesmu/vidarr/VidarrAction.java
+++ plugin-vidarr/src/main/java/ca/on/oicr/gsi/shesmu/vidarr/VidarrAction.java
@@ -3,11 +3,9 @@ package ca.on.oicr.gsi.shesmu.vidarr;
 import ca.on.oicr.gsi.Pair;
 import ca.on.oicr.gsi.shesmu.plugin.action.Action;
 import ca.on.oicr.gsi.shesmu.plugin.types.Imyhat;
-import ca.on.oicr.gsi.vidarr.api.VidarrRequest;
 import java.util.stream.Stream;
 
 public abstract class VidarrAction extends Action {
-  VidarrRequest request;
   static final Imyhat EXTERNAL_IDS =
       new Imyhat.ObjectImyhat(
               Stream.of(new Pair<>("id", Imyhat.STRING), new Pair<>("provider", Imyhat.STRING)))
